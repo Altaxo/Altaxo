b4f773df20ca93b78f77375a8a86294ec9c6c6e9
Dirk Lellinger <dlellinger@users.sourceforge.net>
2003-09-30 18:27:40 +0000

    Move the directory structure (2)
    --HG--
    extra : convert_revision : svn%3Ad1681fee-8e23-fc4c-b001-5eb8f207e95b/trunk%40108

---
+++ src/Altaxo/Startup/AltaxoStartup.cs
@@ -0,0 +1,19 @@
+using System;
+using Altaxo;
+using Altaxo.Main;
+
+
+namespace AltaxoStartup
+{
+	class Startup
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			App.Main();
+		}
+	}
+}
