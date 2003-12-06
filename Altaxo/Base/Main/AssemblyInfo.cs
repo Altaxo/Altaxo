8c4b944a82f3930e02f790f32528540e9b7998c2
Dirk Lellinger <dlellinger@users.sourceforge.net>
2003-12-06 22:37:32 +0000

    Try to set Revision on AssemblyInfo
    --HG--
    extra : convert_revision : svn%3Ad1681fee-8e23-fc4c-b001-5eb8f207e95b/trunk%40177

--- Altaxo/Base/Main/AssemblyInfo.cs
+++ Altaxo/Base/Main/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using Altaxo.Serialization;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyVersion("0.5.$Rev$.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
