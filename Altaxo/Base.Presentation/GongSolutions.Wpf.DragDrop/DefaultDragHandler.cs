2b364b3069bc592b5428f2d6dd11c66ab698858e
Dirk Lellinger <dlellinger@users.sourceforge.net>
2014-11-09 17:22:28 +0100

    Integrate Gongsolutions WPF drag drop library into AltaxoBase.Presentations

---
+++ Altaxo/Base.Presentation/GongSolutions.Wpf.DragDrop/DefaultDragHandler.cs
@@ -0,0 +1,40 @@
+﻿using GongSolutions.Wpf.DragDrop.Utilities;
+using System.Linq;
+using System.Windows;
+
+namespace GongSolutions.Wpf.DragDrop
+{
+	public class DefaultDragHandler : IDragSource
+	{
+		public virtual void StartDrag(IDragInfo dragInfo)
+		{
+			var itemCount = dragInfo.SourceItems.Cast<object>().Count();
+
+			if (itemCount == 1)
+			{
+				dragInfo.Data = dragInfo.SourceItems.Cast<object>().First();
+			}
+			else if (itemCount > 1)
+			{
+				dragInfo.Data = TypeUtilities.CreateDynamicallyTypedList(dragInfo.SourceItems);
+			}
+
+			dragInfo.Effects = (dragInfo.Data != null) ?
+													 DragDropEffects.Copy | DragDropEffects.Move :
+													 DragDropEffects.None;
+		}
+
+		public bool CanStartDrag(IDragInfo dragInfo)
+		{
+			return true;
+		}
+
+		public virtual void Dropped(IDropInfo dropInfo)
+		{
+		}
+
+		public virtual void DragCancelled()
+		{
+		}
+	}
+}
