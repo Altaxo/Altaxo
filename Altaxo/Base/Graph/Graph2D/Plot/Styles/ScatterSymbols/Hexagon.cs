880668c3cadcf20103e3e402dfe4ce0aab454107
Dirk Lellinger <dlellinger@users.sourceforge.net>
2016-11-10 22:20:01 +0100

    Add Pentagon and Hexagon symbol shapes

---
+++ Altaxo/Base/Graph/Graph2D/Plot/Styles/ScatterSymbols/Hexagon.cs
@@ -0,0 +1,86 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2016 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using Altaxo.Drawing;
+using Altaxo.Geometry;
+using ClipperLib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+
+namespace Altaxo.Graph.Graph2D.Plot.Styles.ScatterSymbols
+{
+	public class Hexagon : ClosedSymbolBase
+	{
+		#region Serialization
+
+		/// <summary>
+		/// 2016-10-27 initial version.
+		/// </summary>
+		[Altaxo.Serialization.Xml.XmlSerializationSurrogateFor(typeof(Hexagon), 0)]
+		private class XmlSerializationSurrogate0 : Altaxo.Serialization.Xml.IXmlSerializationSurrogate
+		{
+			public void Serialize(object obj, Altaxo.Serialization.Xml.IXmlSerializationInfo info)
+			{
+				info.AddBaseValueEmbedded(obj, obj.GetType().BaseType);
+
+				SerializeSetV0((IScatterSymbol)obj, info);
+			}
+
+			public object Deserialize(object o, Altaxo.Serialization.Xml.IXmlDeserializationInfo info, object parent)
+			{
+				var s = (Hexagon)o ?? new Hexagon();
+				info.GetBaseValueEmbedded(s, s.GetType().BaseType, parent);
+
+				return DeserializeSetV0(s, info, parent);
+			}
+		}
+
+		#endregion Serialization
+
+		public Hexagon()
+		{
+		}
+
+		public Hexagon(NamedColor fillColor, bool isFillColorInfluencedByPlotColor)
+			: base(fillColor, isFillColorInfluencedByPlotColor)
+		{
+		}
+
+		public override List<List<ClipperLib.IntPoint>> GetCopyOfOuterPolygon()
+		{
+			var list = new List<ClipperLib.IntPoint>(6);
+			for (int i = 0; i < 6; ++i)
+			{
+				var phi = Math.PI * (i / 3.0);
+				list.Add(new IntPoint((int)(ClipperScalingInt * Math.Cos(phi)), (int)(ClipperScalingInt * Math.Sin(phi))));
+			}
+
+			return new List<List<ClipperLib.IntPoint>>(1) { list };
+		}
+	}
+}
