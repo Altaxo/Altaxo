eaf972d4ee51ae310bfb5c822c23abc384ff70bb
Dirk Lellinger <dlellinger@users.sourceforge.net>
2018-04-14 22:24:43 +0200

    Add VS2013 themes

--- Altaxo/Workbench/Gui/MenuCommands/HelpCommands.cs
+++ Altaxo/Workbench/Gui/MenuCommands/HelpCommands.cs
@@ -7,45 +7,72 @@ using System.Threading.Tasks;
 
 namespace Altaxo.Gui.MenuCommands
 {
-	public class DevelopmentTest : SimpleCommand
+  public class DevelopmentTest : SimpleCommand
+  {
+	public override void Execute(object parameter)
 	{
-		public override void Execute(object parameter)
-		{
-			Run6();
-		}
-
-		public void Run6()
-		{
-			GC.Collect();
-			System.Threading.Thread.Sleep(1000);
-			var comMananger = Altaxo.Current.ComManager;
-		}
+	  Run6();
 	}
 
-	public class LoadVS2010Theme : SimpleCommand
+	public void Run6()
 	{
-		public override void Execute(object parameter)
-		{
-			var wb = Current.GetRequiredService<AltaxoWorkbench>();
-			wb.DockManagerTheme = "vs2010";
-		}
+	  GC.Collect();
+	  System.Threading.Thread.Sleep(1000);
+	  var comMananger = Altaxo.Current.ComManager;
 	}
+  }
 
-	public class LoadAeroTheme : SimpleCommand
+  public class LoadVS2010Theme : SimpleCommand
+  {
+	public override void Execute(object parameter)
 	{
-		public override void Execute(object parameter)
-		{
-			var wb = Current.GetRequiredService<AltaxoWorkbench>();
-			wb.DockManagerTheme = "aero";
-		}
+	  var wb = Current.GetRequiredService<AltaxoWorkbench>();
+	  wb.DockManagerTheme = "vs2010";
 	}
+  }
 
-	public class LoadMetroTheme : SimpleCommand
+  public class LoadAeroTheme : SimpleCommand
+  {
+	public override void Execute(object parameter)
 	{
-		public override void Execute(object parameter)
-		{
-			var wb = Current.GetRequiredService<AltaxoWorkbench>();
-			wb.DockManagerTheme = "metro";
-		}
+	  var wb = Current.GetRequiredService<AltaxoWorkbench>();
+	  wb.DockManagerTheme = "aero";
 	}
+  }
+
+  public class LoadMetroTheme : SimpleCommand
+  {
+	public override void Execute(object parameter)
+	{
+	  var wb = Current.GetRequiredService<AltaxoWorkbench>();
+	  wb.DockManagerTheme = "metro";
+	}
+  }
+
+  public class LoadVS2013DarkTheme : SimpleCommand
+  {
+	public override void Execute(object parameter)
+	{
+	  var wb = Current.GetRequiredService<AltaxoWorkbench>();
+	  wb.DockManagerTheme = "vs2013dark";
+	}
+  }
+
+  public class LoadVS2013LightTheme : SimpleCommand
+  {
+	public override void Execute(object parameter)
+	{
+	  var wb = Current.GetRequiredService<AltaxoWorkbench>();
+	  wb.DockManagerTheme = "vs2013light";
+	}
+  }
+
+  public class LoadVS2013BlueTheme : SimpleCommand
+  {
+	public override void Execute(object parameter)
+	{
+	  var wb = Current.GetRequiredService<AltaxoWorkbench>();
+	  wb.DockManagerTheme = "vs2013blue";
+	}
+  }
 }
