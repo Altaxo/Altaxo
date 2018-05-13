44c1f662aed3399acf4bdb57d047ddd06198dde0
Dirk Lellinger <dlellinger@users.sourceforge.net>
2018-05-13 23:15:05 +0200

    Add provider for parsing and representing a LaTeX formla as an png image stream

---
+++ Altaxo/Base/Main/Services/ILaTeXFormulaImageStreamProvider.cs
@@ -0,0 +1,47 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2018 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altaxo.Main.Services
+{
+	/// <summary>
+	/// Interface to provide a LaTeX formula as an image. The image is provided as a stream, representing a .PNG image.
+	/// </summary>
+	public interface ILaTeXFormulaImageStreamProvider
+	{
+		/// <summary>
+		/// Parses the specified LaTeX formula text.
+		/// </summary>
+		/// <param name="formulaText">The formula text.</param>
+		/// <param name="fontSize">Size of the font in which the formula text is embedded.</param>
+		/// <param name="dpiResolution">The resolution of the required image.</param>
+		/// <returns>A tuple consisting of the bitmap stream, and the html attribute how to place the image.</returns>
+		(System.IO.Stream bitmapStream, string placement) Parse(string formulaText, double fontSize, double dpiResolution);
+	}
+}
---
+++ Altaxo/Dom.Presentation/Gui/Text/Viewing/LaTeXFormulaImageStreamProvider.cs
@@ -0,0 +1,72 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2018 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WpfMath;
+
+namespace Altaxo.Gui.Markdown
+{
+	/// <summary>
+	/// This class converts LaTeX formulas to an image (as a framework independent stream).
+	/// </summary>
+	public class LaTeXFormulaImageStreamProvider : Altaxo.Main.Services.ILaTeXFormulaImageStreamProvider
+	{
+		private static TexFormulaParser formulaParser = new TexFormulaParser();
+		private static Pen pen = new Pen(Brushes.Black, 1);
+
+		public (Stream bitmapStream, string placement) Parse(string text, double fontSize, double dpiResolution)
+		{
+			TexFormula formula = null;
+			try
+			{
+				formula = formulaParser.Parse(text);
+			}
+			catch (Exception)
+			{
+				return (null, null);
+			}
+
+			var formulaRenderer = formula.GetRenderer(TexStyle.Display, fontSize, "Arial");
+			// the placement of the image depends on the depth value
+			var absoluteDepth = formulaRenderer.RenderSize.Height * formulaRenderer.RelativeDepth;
+
+			var bmp = formulaRenderer.RenderToBitmap(0, 0);
+
+			var fileStream = new MemoryStream();
+			BitmapEncoder encoder = new PngBitmapEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(bmp));
+			encoder.Save(fileStream);
+			fileStream.Seek(0, SeekOrigin.Begin);
+
+			return (fileStream, "baseline");
+		}
+	}
+}
