f37832a8a8159113321122742f74c96d4308c42d
Dirk Lellinger <dlellinger@users.sourceforge.net>
2019-05-08 23:27:31 +0200

    Adds a controller for controlling the smoothness of a smoothing cubic spline

---
+++ Altaxo/Base/Gui/Worksheet/SmoothingCubicSplineController.cs
@@ -0,0 +1,67 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2019 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+using Altaxo.Calc.Interpolation;
+using Altaxo.Gui.Common;
+
+namespace Altaxo.Gui.Worksheet
+{
+  /// <summary>
+  /// Controls the Smoothing parameter of a rational cubic spline.
+  /// </summary>
+  [UserControllerForObject(typeof(Altaxo.Calc.Interpolation.SmoothingCubicSpline), 100)]
+  public class SmoothingCubicSplineController : NumericDoubleValueController
+  {
+    private SmoothingCubicSpline _spline;
+
+    public SmoothingCubicSplineController(SmoothingCubicSpline spline)
+      : base(spline.Smoothness)
+    {
+      base._minimumValue = 0;
+      base._isMinimumValueIncluded = true;
+      _descriptionText = "Smoothness (0-Infinity, 0: cubic spline, infinity: linear regression)  :";
+      _spline = spline;
+    }
+
+    public override object ModelObject
+    {
+      get
+      {
+        return _spline;
+      }
+    }
+
+    public override bool Apply(bool disposeController)
+    {
+      if (base.Apply(disposeController))
+      {
+        _spline.Smoothness = base._value1Double;
+        return true;
+      }
+      else
+        return false;
+    }
+  }
+}
