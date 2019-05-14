1da25a13d17ea222eb98e0737a711be80ab358a0
Dirk Lellinger <dlellinger@users.sourceforge.net>
2019-05-14 22:34:58 +0200

    Use .nuget package references

--- debugbuild.bat
+++ debugbuild.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q .\rtDir
 @IF exist .\rtDir GOTO err
 mkdir rtDir
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe" /m Altaxo.sln /t:Restore;Build /p:Configuration=Debug "/p:Platform=Any CPU"
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe" /m Altaxo.sln /t:build -restore /p:Configuration=Debug "/p:Platform=Any CPU"
 IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 :err
