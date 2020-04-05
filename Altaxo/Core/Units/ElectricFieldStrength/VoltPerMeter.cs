728b96d6c152bf9c0fe158922532d770662d7aaf
Dirk Lellinger <dlellinger@users.sourceforge.net>
2020-04-05 22:36:17 +0200

    Add some electrical units

---
+++ Altaxo/Core/Units/ElectricFieldStrength/VoltPerMeter.cs
@@ -0,0 +1,60 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2017 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altaxo.Units.ElectricFieldStrength
+{
+  [UnitDescription("Electric field strength", 1, 1, -3, -1, 0, 0, 0)]
+  public class VoltPerMeter : SIUnit
+  {
+    private static readonly VoltPerMeter _instance = new VoltPerMeter();
+
+    public static VoltPerMeter Instance { get { return _instance; } }
+
+    private VoltPerMeter()
+        : base(1, 1, -3, -1, 0, 0, 0)
+    {
+    }
+
+    public override string Name
+    {
+      get { return "Volt per meter"; }
+    }
+
+    public override string ShortCut
+    {
+      get { return "V/m"; }
+    }
+
+    public override ISIPrefixList Prefixes
+    {
+      get { return SIPrefix.ListWithAllKnownPrefixes; }
+    }
+  }
+}
