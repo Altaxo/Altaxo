b6c90025a96ee86d12c17ed83ef1403534bec86c
Dirk Lellinger <dlellinger@users.sourceforge.net>
2020-04-25 23:46:59 +0200

    Remove verbose report of assembly loading from Runtime

--- Altaxo/Base/AddInItems/AddIn/Runtime.cs
+++ Altaxo/Base/AddInItems/AddIn/Runtime.cs
@@ -16,9 +16,6 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-#define VerboseInfo_AssemblyLoading
-
-
 using System;
 using System.Collections.Generic;
 using System.IO;
