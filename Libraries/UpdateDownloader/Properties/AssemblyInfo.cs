9f1bc1c3b40d6b1624f79ab7612ac2da94b0c7df
Dirk Lellinger <dlellinger@users.sourceforge.net>
2020-04-27 23:01:22 +0200

    Bring project AutoUpdateDownloader to new project format

---
+++ Libraries/UpdateDownloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿#region Copyright
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2013 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AltaxoUpdateDownloader")]
+[assembly: AssemblyDescription("Downloader for Altaxo's auto update files")]
+[assembly: AssemblyConfiguration("REVID: $REVID$, BRANCH: $BRANCH$, DATE: $REVDATE$")]
+[assembly: AssemblyCompany("http://altaxo.sourceforge.net")]
+[assembly: AssemblyProduct("Altaxo")]
+[assembly: AssemblyCopyright("(C) Dr. Dirk Lellinger 2002-$YEAR$")]
+[assembly: AssemblyTrademark("(C) Dr. Dirk Lellinger 2002-$YEAR$")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("faa9e96d-cecc-41d8-8400-0c106e64f226")]
+
+[assembly: AssemblyVersion("$MAJORVERSION$.$MINORVERSION$.$REVNUM$.$DIRTY$")]
+[assembly: AssemblyFileVersion("$MAJORVERSION$.$MINORVERSION$.$REVNUM$.$DIRTY$")]
