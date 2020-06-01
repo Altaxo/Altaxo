e050ed06164bba8dbfb8ae7c015f39e831bcc75d
Dirk Lellinger <dlellinger@users.sourceforge.net>
2020-06-01 11:32:49 +0200

    Get rid of binaries that are now included by PackageReferences

--- debugbuild.cmd
+++ debugbuild.cmd
@@ -6,7 +6,7 @@ rmdir /S /Q .\rtnetcoreapp3.1
 mkdir rtnet48
 mkdir rtnetcoreapp3.1
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe" /m Altaxo.sln /t:build -restore /p:Configuration=Debug "/p:Platform=Any CPU"
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe" /m Altaxo.sln /t:Restore;Build /p:Configuration=Debug "/p:Platform=Any CPU"
 IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 :err
