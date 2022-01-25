723a558973a7144171152087aad88163d71e495a
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-01-25 22:49:59 +0100

    Make GridPlaneController data context aware

--- Altaxo/Base/Gui/Graph/Gdi/Axis/GridPlaneController.cs
+++ Altaxo/Base/Gui/Graph/Gdi/Axis/GridPlaneController.cs
@@ -33,24 +33,103 @@ using Altaxo.Gui.Common.Drawing;
 
 namespace Altaxo.Gui.Graph.Gdi.Axis
 {
-  [ExpectedTypeOfView(typeof(IMultiChildView))]
+  public interface IGridPlanView : IDataContextAwareView { }
+
+  [ExpectedTypeOfView(typeof(IGridPlanView))]
   [UserControllerForObject(typeof(GridPlane))]
-  public class GridPlaneController : MVCANControllerEditOriginalDocBase<GridPlane, IMultiChildView>
+  public class GridPlaneController : MVCANControllerEditOriginalDocBase<GridPlane, IGridPlanView>
   {
-    private MultiChildController _innerController;
-
-    private IMVCANController _grid1;
-    private IMVCANController _grid2;
-    private IMVCANController _background;
-
     public override IEnumerable<ControllerAndSetNullMethod> GetSubControllers()
     {
       yield return new ControllerAndSetNullMethod(_grid1, () => _grid1 = null);
       yield return new ControllerAndSetNullMethod(_grid2, () => _grid2 = null);
       yield return new ControllerAndSetNullMethod(_background, () => _background = null);
-
-      yield return new ControllerAndSetNullMethod(_innerController, () => _innerController = null);
     }
+
+    #region Bindings
+
+    private IMVCANController _grid1;
+
+    public IMVCANController Grid1Controller
+    {
+      get => _grid1;
+      set
+      {
+        if (!(_grid1 == value))
+        {
+          _grid1 = value;
+          OnPropertyChanged(nameof(Grid1Controller));
+        }
+      }
+    }
+
+    private string _Grid1Name;
+
+    public string Grid1Name
+    {
+      get => _Grid1Name;
+      set
+      {
+        if (!(_Grid1Name == value))
+        {
+          _Grid1Name = value;
+          OnPropertyChanged(nameof(Grid1Name));
+        }
+      }
+    }
+
+
+    private IMVCANController _grid2;
+
+    public IMVCANController Grid2Controller
+    {
+      get => _grid2;
+      set
+      {
+        if (!(_grid2 == value))
+        {
+          _grid2 = value;
+          OnPropertyChanged(nameof(Grid2Controller));
+        }
+      }
+    }
+
+    private string _Grid2Name;
+
+    public string Grid2Name
+    {
+      get => _Grid2Name;
+      set
+      {
+        if (!(_Grid2Name == value))
+        {
+          _Grid2Name = value;
+          OnPropertyChanged(nameof(Grid2Name));
+        }
+      }
+    }
+
+
+    private IMVCANController _background;
+
+    public IMVCANController BackgroundController
+    {
+      get => _background;
+      set
+      {
+        if (!(_background == value))
+        {
+          _background = value;
+          OnPropertyChanged(nameof(BackgroundController));
+          OnPropertyChanged(nameof(BackgroundView));
+        }
+      }
+    }
+
+    public object? BackgroundView => _background?.ViewObject;
+
+
+    #endregion
 
     protected override void Initialize(bool initData)
     {
@@ -58,29 +137,31 @@ namespace Altaxo.Gui.Graph.Gdi.Axis
 
       if (initData)
       {
-        _grid1 = new XYGridStyleController() { UseDocumentCopy = UseDocument.Directly };
-        _grid1.InitializeDocument(_doc.GridStyleFirst ?? new GridStyle() { ShowGrid = false });
-        Current.Gui.FindAndAttachControlTo(_grid1);
-        var c1 = new ControlViewElement(GridName(_doc.PlaneID.InPlaneAxisNumber1), _grid1, _grid1.ViewObject);
+        var grid1 = new XYGridStyleController() { UseDocumentCopy = UseDocument.Directly };
+        grid1.InitializeDocument(_doc.GridStyleFirst ?? new GridStyle() { ShowGrid = false });
+        Grid1Controller = grid1;
+        Grid1Name = GridName(_doc.PlaneID.InPlaneAxisNumber1);
 
-        _grid2 = new XYGridStyleController() { UseDocumentCopy = UseDocument.Directly };
-        _grid2.InitializeDocument(_doc.GridStyleSecond ?? new GridStyle() { ShowGrid = false });
-        Current.Gui.FindAndAttachControlTo(_grid2);
-        var c2 = new ControlViewElement(GridName(_doc.PlaneID.InPlaneAxisNumber2), _grid2, _grid2.ViewObject);
+        var grid2 = new XYGridStyleController() { UseDocumentCopy = UseDocument.Directly };
+        grid2.InitializeDocument(_doc.GridStyleSecond ?? new GridStyle() { ShowGrid = false });
+        Grid2Controller = grid2;
+        Grid2Name = GridName(_doc.PlaneID.InPlaneAxisNumber2);
 
-        _background = new BrushControllerAdvanced() { UseDocumentCopy = UseDocument.Directly };
-        _background.InitializeDocument(_doc.Background ?? BrushX.Empty);
-        Current.Gui.FindAndAttachControlTo(_background);
-        var c3 = new ControlViewElement("Background", _background, _background.ViewObject);
-
-        _innerController = new MultiChildController(new ControlViewElement[] { c1, c2, c3 }, false);
+        var background = new BrushControllerAdvanced() { UseDocumentCopy = UseDocument.Directly };
+        background.InitializeDocument(_doc.Background ?? BrushX.Empty);
+        Current.Gui.FindAndAttachControlTo(background);
+        BackgroundController = background;
       }
     }
 
     public override bool Apply(bool disposeController)
     {
-      if (false == _innerController.Apply(disposeController))
-        return false;
+      if (!_grid1.Apply(disposeController))
+        return ApplyEnd(false, disposeController);
+      if (!_grid2.Apply(disposeController))
+        return ApplyEnd(false, disposeController);
+      if (!_background.Apply(disposeController))
+        return ApplyEnd(false, disposeController);
 
       _doc.GridStyleFirst = (GridStyle)_grid1.ModelObject;
       _doc.GridStyleSecond = (GridStyle)_grid2.ModelObject;
@@ -89,25 +170,7 @@ namespace Altaxo.Gui.Graph.Gdi.Axis
 
       return ApplyEnd(true, disposeController);
     }
-
-    protected override void AttachView()
-    {
-      base.AttachView();
-      if (_innerController is not null)
-      {
-        _innerController.ViewObject = _view;
-      }
-    }
-
-    protected override void DetachView()
-    {
-      if (_innerController is not null)
-      {
-        _innerController.ViewObject = null;
-      }
-      base.DetachView();
-    }
-
+   
     private static string GridName(int axisNumber)
     {
       switch (axisNumber)
