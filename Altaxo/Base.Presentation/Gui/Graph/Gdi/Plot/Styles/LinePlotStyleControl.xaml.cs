33c588dce1f2d69c9e880ae1cda26bb9628f2608
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-02-19 22:40:01 +0100

    Make LinePlotStyleController data context aware

--- Altaxo/Base.Presentation/Gui/Graph/Gdi/Plot/Styles/LinePlotStyleControl.xaml.cs
+++ Altaxo/Base.Presentation/Gui/Graph/Gdi/Plot/Styles/LinePlotStyleControl.xaml.cs
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //    Altaxo:  a data processing and data plotting program
-//    Copyright (C) 2002-2011 Dr. Dirk Lellinger
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -22,204 +22,18 @@
 
 #endregion Copyright
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
 
 namespace Altaxo.Gui.Graph.Gdi.Plot.Styles
 {
-  using Altaxo.Collections;
-  using Altaxo.Drawing;
-  using Common.Drawing;
-
   /// <summary>
   /// Interaction logic for XYPlotLineStyleControl.xaml
   /// </summary>
   public partial class LinePlotStyleControl : UserControl, ILinePlotStyleView
   {
-    private PenControlsGlue _linePenGlue;
-
-    public event Action? IndependentLineColorChanged;
-
-    public event Action? UseLineChanged;
-
-    public event Action? LinePenChanged;
-
     public LinePlotStyleControl()
     {
       InitializeComponent();
-
-      _linePenGlue = new PenControlsGlue(false);
-      _linePenGlue.PenChanged += new EventHandler(EhLinePenChanged);
-      _linePenGlue.CbBrush = _guiLineBrush;
-      _linePenGlue.CbDashPattern = _guiLineDashStyle;
-      _linePenGlue.CbLineThickness = _guiLineThickness1;
     }
-
-    #region Event handlers
-
-    private void EhIndependentDashStyleChanged(object sender, RoutedEventArgs e)
-    {
-    }
-
-    private void EhUseLineConnectChanged(object sender, RoutedEventArgs e)
-    {
-      GuiHelper.SynchronizeSelectionFromGui(_guiLineConnect);
-      if (_guiLineConnect.SelectedItem is not null) // null for SelectedItem can happen when the DataSource is chaning
-        UseLineChanged?.Invoke();
-    }
-
-    private void EhIndependentLineColorChanged(object sender, RoutedEventArgs e)
-    {
-      IndependentLineColorChanged?.Invoke();
-    }
-
-    private void EhLinePenChanged(object? sender, EventArgs e)
-    {
-      LinePenChanged?.Invoke();
-    }
-
-    #endregion Event handlers
-
-    #region IXYPlotLineStyleView
-
-    #region Line pen
-
-    public bool IndependentLineColor
-    {
-      get
-      {
-        return true == _guiIndependentLineColor.IsChecked;
-      }
-      set
-      {
-        _guiIndependentLineColor.IsChecked = value;
-      }
-    }
-
-    public bool IndependentDashStyle
-    {
-      get
-      {
-        return true == _guiIndependentDashStyle.IsChecked;
-      }
-      set
-      {
-        _guiIndependentDashStyle.IsChecked = value;
-      }
-    }
-
-    public bool ShowPlotColorsOnlyForLinePen
-    {
-      set { _linePenGlue.ShowPlotColorsOnly = value; }
-    }
-
-    public PenX LinePen
-    {
-      get
-      {
-        return _linePenGlue.Pen;
-      }
-      set
-      {
-        if (value is null)
-          throw new ArgumentNullException("FramePen");
-        _linePenGlue.Pen = value;
-      }
-    }
-
-    #endregion Line pen
-
-    public bool IndependentSymbolSize
-    {
-      get
-      {
-        return true == _guiIndependentSymbolSize.IsChecked;
-      }
-
-      set
-      {
-        _guiIndependentSymbolSize.IsChecked = value;
-      }
-    }
-
-    public double SymbolSize
-    {
-      get
-      {
-        return _guiSymbolSize.SelectedQuantityAsValueInPoints;
-      }
-      set
-      {
-        _guiSymbolSize.SelectedQuantityAsValueInPoints = value;
-      }
-    }
-
-    public bool UseSymbolGap
-    {
-      get
-      {
-        return _guiUseLineSymbolGap.IsChecked == true;
-      }
-
-      set
-      {
-        _guiUseLineSymbolGap.IsChecked = value;
-      }
-    }
-
-    public double SymbolGapOffset
-    {
-      get
-      {
-        return _guiSymbolGapOffset.SelectedQuantityAsValueInPoints;
-      }
-
-      set
-      {
-        _guiSymbolGapOffset.SelectedQuantityAsValueInPoints = value;
-      }
-    }
-
-    public double SymbolGapFactor
-    {
-      get
-      {
-        return _guiSymbolGapFactor.SelectedQuantityAsValueInSIUnits;
-      }
-
-      set
-      {
-        _guiSymbolGapFactor.SelectedQuantityAsValueInSIUnits = value;
-      }
-    }
-
-    public void InitializeLineConnect(SelectableListNodeList list)
-    {
-      GuiHelper.Initialize(_guiLineConnect, list);
-    }
-
-    public bool ConnectCircular
-    {
-      get { return true == _guiConnectCircular.IsChecked; }
-      set { _guiConnectCircular.IsChecked = value; }
-    }
-
-    public bool IgnoreMissingDataPoints
-    {
-      get { return true == _guiIgnoreMissingPoints.IsChecked; }
-      set { _guiIgnoreMissingPoints.IsChecked = value; }
-    }
-
-    public bool IndependentOnShiftingGroupStyles
-    {
-      get { return true == _guiIndependentOnShiftingGroupStyles.IsChecked; }
-      set { _guiIndependentOnShiftingGroupStyles.IsChecked = value; }
-    }
-
-    #endregion IXYPlotLineStyleView
   }
 }
--- Altaxo/Base/Gui/Graph/Gdi/Plot/Styles/LinePlotStyleController.cs
+++ Altaxo/Base/Gui/Graph/Gdi/Plot/Styles/LinePlotStyleController.cs
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //    Altaxo:  a data processing and data plotting program
-//    Copyright (C) 2002-2011 Dr. Dirk Lellinger
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -32,61 +32,23 @@ using Altaxo.Graph;
 using Altaxo.Graph.Gdi.Plot.Styles;
 using Altaxo.Graph.Gdi.Plot.Styles.LineConnectionStyles;
 using Altaxo.Graph.Plot.Groups;
+using Altaxo.Gui.Common;
+using Altaxo.Gui.Common.Drawing;
 using Altaxo.Gui.Graph;
 using Altaxo.Gui.Graph.Plot.Groups;
 using Altaxo.Main;
+using Altaxo.Units;
 
 namespace Altaxo.Gui.Graph.Gdi.Plot.Styles
 {
-  #region Interfaces
-
+  
   /// <summary>
   /// This view interface is for showing the options of the XYZPlotLineStyle
   /// </summary>
-  public interface ILinePlotStyleView
+  public interface ILinePlotStyleView : IDataContextAwareView
   {
-    bool ShowPlotColorsOnlyForLinePen { set; }
-
-    bool IndependentLineColor { get; set; }
-
-    bool IndependentDashStyle { get; set; }
-
-    PenX LinePen { get; set; }
-
-    bool IndependentSymbolSize { get; set; }
-
-    double SymbolSize { get; set; }
-
-    bool UseSymbolGap { get; set; }
-
-    double SymbolGapOffset { get; set; }
-
-    double SymbolGapFactor { get; set; }
-
-    bool ConnectCircular { get; set; }
-
-    bool IgnoreMissingDataPoints { get; set; }
-
-    bool IndependentOnShiftingGroupStyles { get; set; }
-
-    /// <summary>
-    /// Initializes the Line connection combobox.
-    /// </summary>
-    /// <param name="list">List of possible selections.</param>
-    void InitializeLineConnect(SelectableListNodeList list);
-
-    #region events
-
-    /// <summary>Occurs when the user choice for IndependentColor of the frame pen has changed.</summary>
-    event Action IndependentLineColorChanged;
-
-    /// <summary>Occurs when the user checked or unchecked the "use frame" checkbox.</summary>
-    event Action UseLineChanged;
-
-    #endregion events
   }
 
-  #endregion Interfaces
 
   /// <summary>
   /// Summary description for XYPlotLineStyleController.
@@ -98,12 +60,221 @@ namespace Altaxo.Gui.Graph.Gdi.Plot.Styles
     /// <summary>Tracks the presence of a color group style in the parent collection.</summary>
     private ColorGroupStylePresenceTracker _colorGroupStyleTracker;
 
-    private SelectableListNodeList _lineConnectChoices;
-
     public override IEnumerable<ControllerAndSetNullMethod> GetSubControllers()
     {
-      yield break;
+      yield return new ControllerAndSetNullMethod(_linePen, () => LinePen = null);
     }
+
+    #region Bindings
+
+    private ItemsController<Type> _lineConnectChoices;
+
+    public ItemsController<Type> LineConnectChoices
+    {
+      get => _lineConnectChoices;
+      set
+      {
+        if (!(_lineConnectChoices == value))
+        {
+          _lineConnectChoices = value;
+          OnPropertyChanged(nameof(LineConnectChoices));
+        }
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the line is shown or not. By definition here, the line is not shown only if the connection style is "Noline".
+    /// When setting this property, this influences not the connection style in the _view, but only the IsEnabled property of all Gui items associated with the line.
+    /// </summary>
+    /// <value>
+    /// 	<c>true</c> if the line used; otherwise, <c>false</c>.
+    /// </value>
+    public bool IsLineUsed => LineConnectChoices.SelectedValue != typeof(NoConnection);
+
+
+    private bool _independentLineColor;
+
+    public bool IndependentLineColor
+    {
+      get => _independentLineColor;
+      set
+      {
+        if (!(_independentLineColor == value))
+        {
+          _independentLineColor = value;
+          OnPropertyChanged(nameof(IndependentLineColor));
+          EhIndependentLineColorChanged();
+        }
+      }
+    }
+
+    private bool _independentDashStyle;
+
+    public bool IndependentDashStyle
+    {
+      get => _independentDashStyle;
+      set
+      {
+        if (!(_independentDashStyle == value))
+        {
+          _independentDashStyle = value;
+          OnPropertyChanged(nameof(IndependentDashStyle));
+        }
+      }
+    }
+
+
+    private bool _independentSymbolSize;
+
+    public bool IndependentSymbolSize
+    {
+      get => _independentSymbolSize;
+      set
+      {
+        if (!(_independentSymbolSize == value))
+        {
+          _independentSymbolSize = value;
+          OnPropertyChanged(nameof(IndependentSymbolSize));
+        }
+      }
+    }
+
+    private bool _ignoreMissingDataPoints;
+
+    public bool IgnoreMissingDataPoints
+    {
+      get => _ignoreMissingDataPoints;
+      set
+      {
+        if (!(_ignoreMissingDataPoints == value))
+        {
+          _ignoreMissingDataPoints = value;
+          OnPropertyChanged(nameof(IgnoreMissingDataPoints));
+        }
+      }
+    }
+
+
+    private bool _independentOnShiftingGroupStyles;
+
+    public bool IndependentOnShiftingGroupStyles
+    {
+      get => _independentOnShiftingGroupStyles;
+      set
+      {
+        if (!(_independentOnShiftingGroupStyles == value))
+        {
+          _independentOnShiftingGroupStyles = value;
+          OnPropertyChanged(nameof(IndependentOnShiftingGroupStyles));
+        }
+      }
+    }
+
+    private bool _connectCircular;
+
+    public bool ConnectCircular
+    {
+      get => _connectCircular;
+      set
+      {
+        if (!(_connectCircular == value))
+        {
+          _connectCircular = value;
+          OnPropertyChanged(nameof(ConnectCircular));
+        }
+      }
+    }
+
+
+
+    private ColorTypeThicknessPenController _linePen;
+
+    public ColorTypeThicknessPenController LinePen
+    {
+      get => _linePen;
+      set
+      {
+        if (!(_linePen == value))
+        {
+          _linePen?.Dispose();
+          _linePen = value;
+          OnPropertyChanged(nameof(LinePen));
+        }
+      }
+    }
+
+    public QuantityWithUnitGuiEnvironment SymbolSizeEnvironment => LineCapSizeEnvironment.Instance;
+
+
+    private DimensionfulQuantity _symbolSize;
+    /// <summary>
+    /// Initializes the symbol size combobox.
+    /// </summary>
+    public DimensionfulQuantity SymbolSize
+    {
+      get => _symbolSize;
+      set
+      {
+        if (!(_symbolSize == value))
+        {
+          _symbolSize = value;
+          OnPropertyChanged(nameof(SymbolSize));
+        }
+      }
+    }
+
+    private bool _useSymbolGap;
+
+    public bool UseSymbolGap
+    {
+      get => _useSymbolGap;
+      set
+      {
+        if (!(_useSymbolGap == value))
+        {
+          _useSymbolGap = value;
+          OnPropertyChanged(nameof(UseSymbolGap));
+        }
+      }
+    }
+
+    public QuantityWithUnitGuiEnvironment GapOffsetEnvironment => LineCapSizeEnvironment.Instance;
+
+
+    private DimensionfulQuantity _symbolGapOffset;
+
+    public DimensionfulQuantity SymbolGapOffset
+    {
+      get => _symbolGapOffset;
+      set
+      {
+        if (!(_symbolGapOffset == value))
+        {
+          _symbolGapOffset = value;
+          OnPropertyChanged(nameof(SymbolGapOffset));
+        }
+      }
+    }
+
+    public QuantityWithUnitGuiEnvironment GapFactorEnvironment => RelationEnvironment.Instance;
+
+
+    private DimensionfulQuantity _symbolGapFactor;
+
+    public DimensionfulQuantity SymbolGapFactor
+    {
+      get => _symbolGapFactor;
+      set
+      {
+        if (!(_symbolGapFactor == value))
+        {
+          _symbolGapFactor = value;
+          OnPropertyChanged(nameof(SymbolGapFactor));
+        }
+      }
+    }
+
+    #endregion
 
     public override void Dispose(bool isDisposing)
     {
@@ -122,42 +293,38 @@ namespace Altaxo.Gui.Graph.Gdi.Plot.Styles
       {
         _colorGroupStyleTracker = new ColorGroupStylePresenceTracker(_doc, EhColorGroupStyleAddedOrRemoved);
         InitializeLineConnectionChoices();
-      }
 
-      if (_view is not null)
-      {
-        _view.ConnectCircular = _doc.ConnectCircular;
-        _view.IgnoreMissingDataPoints = _doc.IgnoreMissingDataPoints;
-        _view.IndependentOnShiftingGroupStyles = _doc.IndependentOnShiftingGroupStyles;
+        ConnectCircular = _doc.ConnectCircular;
+        IgnoreMissingDataPoints = _doc.IgnoreMissingDataPoints;
+        IndependentOnShiftingGroupStyles = _doc.IndependentOnShiftingGroupStyles;
 
-        // Line properties
-        _view.InitializeLineConnect(_lineConnectChoices);
-        _view.IndependentLineColor = _doc.IndependentLineColor;
-        _view.IndependentDashStyle = _doc.IndependentDashStyle;
-        _view.ShowPlotColorsOnlyForLinePen = _colorGroupStyleTracker.MustUsePlotColorsOnly(_doc.IndependentLineColor);
-        _view.LinePen = _doc.LinePen;
-
-        _view.IndependentSymbolSize = _doc.IndependentSymbolSize;
-        _view.SymbolSize = _doc.SymbolSize;
-        _view.UseSymbolGap = _doc.UseSymbolGap;
-        _view.SymbolGapOffset = _doc.SymbolGapOffset;
-        _view.SymbolGapFactor = _doc.SymbolGapFactor;
+        IndependentLineColor = _doc.IndependentLineColor;
+        IndependentDashStyle = _doc.IndependentDashStyle;
+        LinePen = new ColorTypeThicknessPenController(_doc.LinePen)
+        {
+          ShowPlotColorsOnly = _colorGroupStyleTracker.MustUsePlotColorsOnly(_doc.IndependentLineColor)
+        };
+        
+        IndependentSymbolSize = _doc.IndependentSymbolSize;
+        SymbolSize = new DimensionfulQuantity(_doc.SymbolSize, Altaxo.Units.Length.Point.Instance).AsQuantityIn(SymbolSizeEnvironment.DefaultUnit);
+        UseSymbolGap = _doc.UseSymbolGap;
+        SymbolGapOffset = new DimensionfulQuantity(_doc.SymbolGapOffset, Altaxo.Units.Length.Point.Instance).AsQuantityIn(GapOffsetEnvironment.DefaultUnit);
+        SymbolGapFactor = new DimensionfulQuantity(_doc.SymbolGapFactor, Altaxo.Units.Dimensionless.Unity.Instance).AsQuantityIn(GapFactorEnvironment.DefaultUnit);
       }
     }
 
     private void InitializeLineConnectionChoices()
     {
-      if (_lineConnectChoices is null)
-        _lineConnectChoices = new SelectableListNodeList();
-      else
-        _lineConnectChoices.Clear();
+        var lineConnectChoices = new SelectableListNodeList();
 
       var types = Altaxo.Main.Services.ReflectionService.GetNonAbstractSubclassesOf(typeof(ILineConnectionStyle));
 
       foreach (var t in types)
       {
-        _lineConnectChoices.Add(new SelectableListNode(t.Name, t, t == _doc.Connection.GetType()));
+        lineConnectChoices.Add(new SelectableListNode(t.Name, t, t == _doc.Connection.GetType()));
       }
+
+      LineConnectChoices = new ItemsController<Type>(lineConnectChoices, EhLineConnectionChoiceChanged);
     }
 
     public override bool Apply(bool disposeController)
@@ -165,29 +332,28 @@ namespace Altaxo.Gui.Graph.Gdi.Plot.Styles
       // don't trust user input, so all into a try statement
       try
       {
-        _doc.ConnectCircular = _view.ConnectCircular;
-        _doc.IgnoreMissingDataPoints = _view.IgnoreMissingDataPoints;
-        _doc.IndependentOnShiftingGroupStyles = _view.IndependentOnShiftingGroupStyles;
+        _doc.ConnectCircular = ConnectCircular;
+        _doc.IgnoreMissingDataPoints = IgnoreMissingDataPoints;
+        _doc.IndependentOnShiftingGroupStyles = IndependentOnShiftingGroupStyles;
 
         // Pen
-        _doc.IndependentLineColor = _view.IndependentLineColor;
-        _doc.IndependentDashStyle = _view.IndependentDashStyle;
-        _doc.LinePen = _view.LinePen;
+        _doc.IndependentLineColor = IndependentLineColor;
+        _doc.IndependentDashStyle = IndependentDashStyle;
+        _doc.LinePen = LinePen.Pen;
 
         // Line Connect
 
-        var selNode = _lineConnectChoices.FirstSelectedNode;
-        var connectionType = (Type)(selNode.Tag);
+        var connectionType = LineConnectChoices.SelectedValue;
         if (connectionType == typeof(NoConnection))
           _doc.Connection = NoConnection.Instance;
         else
           _doc.Connection = (ILineConnectionStyle)Activator.CreateInstance(connectionType);
 
-        _doc.IndependentSymbolSize = _view.IndependentSymbolSize;
-        _doc.SymbolSize = _view.SymbolSize;
-        _doc.UseSymbolGap = _view.UseSymbolGap;
-        _doc.SymbolGapOffset = _view.SymbolGapOffset;
-        _doc.SymbolGapFactor = _view.SymbolGapFactor;
+        _doc.IndependentSymbolSize = IndependentSymbolSize;
+        _doc.SymbolSize = SymbolSize.AsValueIn(Altaxo.Units.Length.Point.Instance);
+        _doc.UseSymbolGap = UseSymbolGap;
+        _doc.SymbolGapOffset = SymbolGapOffset.AsValueIn(Altaxo.Units.Length.Point.Instance); ;
+        _doc.SymbolGapFactor = SymbolGapFactor.AsValueInSIUnits;
 
         return ApplyEnd(true, disposeController);
       }
@@ -198,75 +364,36 @@ namespace Altaxo.Gui.Graph.Gdi.Plot.Styles
       }
     }
 
-    protected override void AttachView()
-    {
-      base.AttachView();
-
-      _view.IndependentLineColorChanged += EhIndependentLineColorChanged;
-      _view.UseLineChanged += EhUseLineChanged;
-    }
-
-    protected override void DetachView()
-    {
-      _view.IndependentLineColorChanged -= EhIndependentLineColorChanged;
-      _view.UseLineChanged -= EhUseLineChanged;
-      base.DetachView();
-    }
-
     #region Color management
 
-    /// <summary>
-    /// Gets or sets a value indicating whether the line is shown or not. By definition here, the line is not shown only if the connection style is "Noline".
-    /// When setting this property, this influences not the connection style in the _view, but only the IsEnabled property of all Gui items associated with the line.
-    /// </summary>
-    /// <value>
-    /// 	<c>true</c> if the line used; otherwise, <c>false</c>.
-    /// </value>
-    private bool IsLineUsed
-    {
-      get
-      {
-        var selNode = _lineConnectChoices.FirstSelectedNode;
-        return !NoConnection.Instance.Equals(selNode.Tag);
-      }
-      set
-      {
-        //if(null!=_view)	_view.EnableLineControls = value;
-      }
-    }
+   
 
     private void EhColorGroupStyleAddedOrRemoved()
     {
-      if (_view is not null)
-      {
-        _doc.IndependentLineColor = _view.IndependentLineColor;
+        _doc.IndependentLineColor = IndependentLineColor;
         if (IsLineUsed)
-          _view.ShowPlotColorsOnlyForLinePen = _colorGroupStyleTracker.MustUsePlotColorsOnly(_doc.IndependentLineColor);
-      }
+          LinePen.ShowPlotColorsOnly = _colorGroupStyleTracker.MustUsePlotColorsOnly(_doc.IndependentLineColor);
     }
 
     private void EhIndependentLineColorChanged()
     {
-      if (_view is not null)
-      {
-        _doc.IndependentLineColor = _view.IndependentLineColor;
-        _view.ShowPlotColorsOnlyForLinePen = _colorGroupStyleTracker.MustUsePlotColorsOnly(_doc.IndependentLineColor);
-      }
+        _doc.IndependentLineColor = IndependentLineColor;
+        LinePen.ShowPlotColorsOnly = _colorGroupStyleTracker.MustUsePlotColorsOnly(_doc.IndependentLineColor);
+      
     }
 
-    private void EhUseLineChanged()
+    private void EhLineConnectionChoiceChanged(Type lineConnectionType)
     {
-      var newValue = IsLineUsed;
+      var isLineUsed = lineConnectionType != typeof(NoConnection);
 
-      if (true == newValue)
+      if (isLineUsed)
       {
-        if (_view.LinePen is null || _view.LinePen.IsInvisible)
+        if (LinePen.Pen is null || LinePen.Pen.IsInvisible)
         {
-          _view.LinePen = new PenX(ColorSetManager.Instance.BuiltinDarkPlotColors[0]);
+          LinePen.Pen = new PenX(ColorSetManager.Instance.BuiltinDarkPlotColors[0]);
         }
       }
-
-      IsLineUsed = newValue; // to enable/disable gui items in the control
+      OnPropertyChanged(nameof(IsLineUsed));
     }
 
     #endregion Color management
