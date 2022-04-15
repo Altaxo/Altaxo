af6a3cbe96c231d8f82e3e4816e6db88115a29c9
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-04-15 21:41:20 +0200

    Make HistogramCreationController data context aware

--- Altaxo/Base.Presentation/Gui/Analysis/Statistics/HistogramCreationControl.xaml.cs
+++ Altaxo/Base.Presentation/Gui/Analysis/Statistics/HistogramCreationControl.xaml.cs
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //    Altaxo:  a data processing and data plotting program
-//    Copyright (C) 2002-2015 Dr. Dirk Lellinger
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -22,19 +22,7 @@
 
 #endregion Copyright
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Altaxo.Gui.Analysis.Statistics
 {
@@ -43,187 +31,9 @@ namespace Altaxo.Gui.Analysis.Statistics
   /// </summary>
   public partial class HistogramCreationControl : UserControl, IHistogramCreationView
   {
-    public event Action? BinningTypeChanged;
-
-    public event Action? AutomaticBinningTypeChanged;
-
     public HistogramCreationControl()
     {
       InitializeComponent();
-    }
-
-    public IEnumerable<string> Errors
-    {
-      set { _guiErrors.ItemsSource = value; }
-    }
-
-    public IEnumerable<string> Warnings
-    {
-      set { _guiWarnings.ItemsSource = value; }
-    }
-
-    public double NumberOfValuesOriginal
-    {
-      set { _guiNumberOfValuesOriginal.SelectedValue = value; }
-    }
-
-    public double NumberOfValuesFiltered
-    {
-      set { _guiNumberOfValuesFiltered.SelectedValue = value; }
-    }
-
-    public double NumberOfNaNValues
-    {
-      set { _guiNumberOfNaNValues.SelectedValue = value; }
-    }
-
-    public double NumberOfInfiniteValues
-    {
-      set { _guiNumberOfInfiniteValues.SelectedValue = value; }
-    }
-
-    public double MinimumValue
-    {
-      set { _guiMinimumValue.SelectedValue = value; }
-    }
-
-    public double MaximumValue
-    {
-      set { _guiMaximumValue.SelectedValue = value; }
-    }
-
-    public bool IgnoreNaNValues
-    {
-      get
-      {
-        return true == _guiIgnoreNaNValues.IsChecked;
-      }
-      set
-      {
-        _guiIgnoreNaNValues.IsChecked = value;
-      }
-    }
-
-    public bool IgnoreInfiniteValues
-    {
-      get
-      {
-        return true == _guiIgnoreInfiniteValues.IsChecked;
-      }
-      set
-      {
-        _guiIgnoreInfiniteValues.IsChecked = value;
-      }
-    }
-
-    public bool IgnoreValuesBelowLowerBoundary
-    {
-      get
-      {
-        return true == _guiIgnoreValuesBelowLowerBoundary.IsChecked;
-      }
-      set
-      {
-        _guiIgnoreValuesBelowLowerBoundary.IsChecked = value;
-      }
-    }
-
-    public bool IsLowerBoundaryInclusive
-    {
-      get
-      {
-        return true == _guiIsLowerBoundaryInclusive.IsChecked;
-      }
-      set
-      {
-        _guiIsLowerBoundaryInclusive.IsChecked = value;
-      }
-    }
-
-    public double LowerBoundary
-    {
-      get
-      {
-        return _guiLowerBoundaryToIgnore.SelectedValue;
-      }
-      set
-      {
-        _guiLowerBoundaryToIgnore.SelectedValue = value;
-      }
-    }
-
-    public bool IgnoreValuesAboveUpperBoundary
-    {
-      get
-      {
-        return true == _guiIgnoreValuesAboveUpperBoundary.IsChecked;
-      }
-      set
-      {
-        _guiIgnoreValuesAboveUpperBoundary.IsChecked = value;
-      }
-    }
-
-    public bool IsUpperBoundaryInclusive
-    {
-      get
-      {
-        return true == _guiIsUpperBoundaryInclusive.IsChecked;
-      }
-      set
-      {
-        _guiIsUpperBoundaryInclusive.IsChecked = value;
-      }
-    }
-
-    public double UpperBoundary
-    {
-      get
-      {
-        return _guiUpperBoundaryToIgnore.SelectedValue;
-      }
-      set
-      {
-        _guiUpperBoundaryToIgnore.SelectedValue = value;
-      }
-    }
-
-    public bool UseAutomaticBinning
-    {
-      get
-      {
-        return true == _guiUseAutomaticBinning.IsChecked;
-      }
-      set
-      {
-        _guiUseAutomaticBinning.IsChecked = value;
-      }
-    }
-
-    public Collections.SelectableListNodeList BinningType
-    {
-      set { GuiHelper.Initialize(_guiBinningType, value); }
-    }
-
-    public object BinningView
-    {
-      set { _guiBinningControlHost.Child = value as UIElement; }
-    }
-
-    private void EhBinningTypeChanged(object sender, SelectionChangedEventArgs e)
-    {
-      GuiHelper.SynchronizeSelectionFromGui(_guiBinningType);
-
-      var ev = BinningTypeChanged;
-      if (ev is not null)
-        ev();
-    }
-
-    private void EhAutomaticBinningTypeChanged(object sender, RoutedEventArgs e)
-    {
-      var ev = AutomaticBinningTypeChanged;
-      if (ev is not null)
-        ev();
     }
   }
 }
--- Altaxo/Base/Gui/Analysis/Statistics/HistogramCreationController.cs
+++ Altaxo/Base/Gui/Analysis/Statistics/HistogramCreationController.cs
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //    Altaxo:  a data processing and data plotting program
-//    Copyright (C) 2002-2015 Dr. Dirk Lellinger
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -25,71 +25,305 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
 using Altaxo.Analysis.Statistics.Histograms;
 using Altaxo.Collections;
-using Altaxo.Data;
+using Altaxo.Gui.Common;
 
 namespace Altaxo.Gui.Analysis.Statistics
 {
-  public interface IHistogramCreationView
+  public interface IHistogramCreationView : IDataContextAwareView
   {
-    IEnumerable<string> Errors { set; }
-
-    IEnumerable<string> Warnings { set; }
-
-    double NumberOfValuesOriginal { set; }
-
-    double NumberOfValuesFiltered { set; }
-
-    double NumberOfNaNValues { set; }
-
-    double NumberOfInfiniteValues { set; }
-
-    double MinimumValue { set; }
-
-    double MaximumValue { set; }
-
-    bool IgnoreNaNValues { get; set; }
-
-    bool IgnoreInfiniteValues { get; set; }
-
-    bool IgnoreValuesBelowLowerBoundary { get; set; }
-
-    bool IsLowerBoundaryInclusive { get; set; }
-
-    double LowerBoundary { get; set; }
-
-    bool IgnoreValuesAboveUpperBoundary { get; set; }
-
-    bool IsUpperBoundaryInclusive { get; set; }
-
-    double UpperBoundary { get; set; }
-
-    bool UseAutomaticBinning { get; set; }
-
-    SelectableListNodeList BinningType { set; }
-
-    object BinningView { set; }
-
-    event Action BinningTypeChanged;
-
-    event Action AutomaticBinningTypeChanged;
   }
 
   [UserControllerForObject(typeof(HistogramCreationInformation))]
   [ExpectedTypeOfView(typeof(IHistogramCreationView))]
   public class HistogramCreationController : MVCANControllerEditOriginalDocBase<HistogramCreationInformation, IHistogramCreationView>
   {
-    private IMVCANController _binningController;
-    private SelectableListNodeList _binningTypes = new SelectableListNodeList();
-
     public override IEnumerable<ControllerAndSetNullMethod> GetSubControllers()
     {
       if (_binningController is not null)
-        yield return new ControllerAndSetNullMethod(_binningController, () => _binningController = null);
+        yield return new ControllerAndSetNullMethod(_binningController, () => BinningController = null);
     }
+
+    #region Bindings
+
+    private ObservableCollection<string> _errors;
+
+    public ObservableCollection<string> Errors
+    {
+      get => _errors;
+      set
+      {
+        if (!(_errors == value))
+        {
+          _errors = value;
+          OnPropertyChanged(nameof(Errors));
+        }
+      }
+    }
+
+    private ObservableCollection<string> _warnings;
+
+    public ObservableCollection<string> Warnings
+    {
+      get => _warnings;
+      set
+      {
+        if (!(_warnings == value))
+        {
+          _warnings = value;
+          OnPropertyChanged(nameof(Warnings));
+        }
+      }
+    }
+
+
+    private double _numberOfValuesOriginal;
+
+    public double NumberOfValuesOriginal
+    {
+      get => _numberOfValuesOriginal;
+      set
+      {
+        if (!(_numberOfValuesOriginal == value))
+        {
+          _numberOfValuesOriginal = value;
+          OnPropertyChanged(nameof(NumberOfValuesOriginal));
+        }
+      }
+    }
+    private double _numberOfValuesFiltered;
+
+    public double NumberOfValuesFiltered
+    {
+      get => _numberOfValuesFiltered;
+      set
+      {
+        if (!(_numberOfValuesFiltered == value))
+        {
+          _numberOfValuesFiltered = value;
+          OnPropertyChanged(nameof(NumberOfValuesFiltered));
+        }
+      }
+    }
+    private double _numberOfNaNValues;
+
+    public double NumberOfNaNValues
+    {
+      get => _numberOfNaNValues;
+      set
+      {
+        if (!(_numberOfNaNValues == value))
+        {
+          _numberOfNaNValues = value;
+          OnPropertyChanged(nameof(NumberOfNaNValues));
+        }
+      }
+    }
+    private double _numberOfInfiniteValues;
+
+    public double NumberOfInfiniteValues
+    {
+      get => _numberOfInfiniteValues;
+      set
+      {
+        if (!(_numberOfInfiniteValues == value))
+        {
+          _numberOfInfiniteValues = value;
+          OnPropertyChanged(nameof(NumberOfInfiniteValues));
+        }
+      }
+    }
+    private double _minimumValue;
+
+    public double MinimumValue
+    {
+      get => _minimumValue;
+      set
+      {
+        if (!(_minimumValue == value))
+        {
+          _minimumValue = value;
+          OnPropertyChanged(nameof(MinimumValue));
+        }
+      }
+    }
+    private double _maximumValue;
+
+    public double MaximumValue
+    {
+      get => _maximumValue;
+      set
+      {
+        if (!(_maximumValue == value))
+        {
+          _maximumValue = value;
+          OnPropertyChanged(nameof(MaximumValue));
+        }
+      }
+    }
+    private bool _ignoreNaNValues;
+    public bool IgnoreNaNValues
+    {
+      get => _ignoreNaNValues;
+      set
+      {
+        if (!(_ignoreNaNValues == value))
+        {
+          _ignoreNaNValues = value;
+          OnPropertyChanged(nameof(IgnoreNaNValues));
+        }
+      }
+    }
+    private bool _ignoreInfiniteValues;
+
+    public bool IgnoreInfiniteValues
+    {
+      get => _ignoreInfiniteValues;
+      set
+      {
+        if (!(_ignoreInfiniteValues == value))
+        {
+          _ignoreInfiniteValues = value;
+          OnPropertyChanged(nameof(IgnoreInfiniteValues));
+        }
+      }
+    }
+    private bool _ignoreValuesBelowLowerBoundary;
+
+    public bool IgnoreValuesBelowLowerBoundary
+    {
+      get => _ignoreValuesBelowLowerBoundary;
+      set
+      {
+        if (!(_ignoreValuesBelowLowerBoundary == value))
+        {
+          _ignoreValuesBelowLowerBoundary = value;
+          OnPropertyChanged(nameof(IgnoreValuesBelowLowerBoundary));
+        }
+      }
+    }
+    private bool _isLowerBoundaryInclusive;
+
+    public bool IsLowerBoundaryInclusive
+    {
+      get => _isLowerBoundaryInclusive;
+      set
+      {
+        if (!(_isLowerBoundaryInclusive == value))
+        {
+          _isLowerBoundaryInclusive = value;
+          OnPropertyChanged(nameof(IsLowerBoundaryInclusive));
+        }
+      }
+    }
+    private double _lowerBoundary;
+
+    public double LowerBoundary
+    {
+      get => _lowerBoundary;
+      set
+      {
+        if (!(_lowerBoundary == value))
+        {
+          _lowerBoundary = value;
+          OnPropertyChanged(nameof(LowerBoundary));
+        }
+      }
+    }
+    private bool _ignoreValuesAboveUpperBoundary;
+
+    public bool IgnoreValuesAboveUpperBoundary
+    {
+      get => _ignoreValuesAboveUpperBoundary;
+      set
+      {
+        if (!(_ignoreValuesAboveUpperBoundary == value))
+        {
+          _ignoreValuesAboveUpperBoundary = value;
+          OnPropertyChanged(nameof(IgnoreValuesAboveUpperBoundary));
+        }
+      }
+    }
+    private bool _isUpperBoundaryInclusive;
+
+    public bool IsUpperBoundaryInclusive
+    {
+      get => _isUpperBoundaryInclusive;
+      set
+      {
+        if (!(_isUpperBoundaryInclusive == value))
+        {
+          _isUpperBoundaryInclusive = value;
+          OnPropertyChanged(nameof(IsUpperBoundaryInclusive));
+        }
+      }
+    }
+    private double _upperBoundary;
+
+    public double UpperBoundary
+    {
+      get => _upperBoundary;
+      set
+      {
+        if (!(_upperBoundary == value))
+        {
+          _upperBoundary = value;
+          OnPropertyChanged(nameof(UpperBoundary));
+        }
+      }
+    }
+    private bool _useAutomaticBinning;
+
+    public bool UseAutomaticBinning
+    {
+      get => _useAutomaticBinning;
+      set
+      {
+        if (!(_useAutomaticBinning == value))
+        {
+          _useAutomaticBinning = value;
+          OnPropertyChanged(nameof(UseAutomaticBinning));
+          EhAutomaticBinningTypeChanged();
+        }
+      }
+    }
+    private ItemsController<Type> _binningTypes;
+
+    public ItemsController<Type> BinningTypes
+    {
+      get => _binningTypes;
+      set
+      {
+        if (!(_binningTypes == value))
+        {
+          _binningTypes = value;
+          OnPropertyChanged(nameof(BinningTypes));
+        }
+      }
+    }
+
+
+    private IMVCANController _binningController;
+
+    public IMVCANController BinningController
+    {
+      get => _binningController;
+      set
+      {
+        if (!(_binningController == value))
+        {
+          _binningController?.Dispose();
+          _binningController = value;
+          OnPropertyChanged(nameof(BinningController));
+        }
+      }
+    }
+
+
+
+    #endregion
 
     protected override void Initialize(bool initData)
     {
@@ -97,63 +331,47 @@ namespace Altaxo.Gui.Analysis.Statistics
 
       if (initData)
       {
-        _binningTypes.Clear();
 
         var binningTypes = Altaxo.Main.Services.ReflectionService.GetNonAbstractSubclassesOf(typeof(IBinning));
-
+        var binningNodes = new SelectableListNodeList();
         foreach (var type in binningTypes)
-          _binningTypes.Add(new SelectableListNode(type.ToString(), type, type == _doc.CreationOptions.Binning.GetType()));
+          binningNodes.Add(new SelectableListNode(type.ToString(), type, type == _doc.CreationOptions.Binning.GetType()));
+        BinningTypes = new ItemsController<Type>(binningNodes, EhBinningTypeChanged);
+        BinningController = (IMVCANController)Current.Gui.GetControllerAndControl(new object[] { _doc.CreationOptions.Binning }, typeof(IMVCANController), UseDocument.Directly);
+        Errors = new ObservableCollection<string>(_doc.Errors);
+        Warnings = new ObservableCollection<string>(_doc.Warnings);
 
-        if (_binningController is not null)
-        {
-          _binningController.Dispose();
-          _binningController = null;
-        }
+        NumberOfValuesOriginal = _doc.NumberOfValuesOriginal;
+        NumberOfValuesFiltered = _doc.NumberOfValuesFiltered;
+        NumberOfNaNValues = _doc.NumberOfNaNValues;
+        NumberOfInfiniteValues = _doc.NumberOfInfiniteValues;
+        MinimumValue = _doc.MinimumValue;
+        MaximumValue = _doc.MaximumValue;
 
-        _binningController = (IMVCANController)Current.Gui.GetControllerAndControl(new object[] { _doc.CreationOptions.Binning }, typeof(IMVCANController), UseDocument.Directly);
-      }
-      if (_view is not null)
-      {
-        _view.Errors = _doc.Errors;
-        _view.Warnings = _doc.Warnings;
+        IgnoreNaNValues = _doc.CreationOptions.IgnoreNaN;
+        IgnoreInfiniteValues = _doc.CreationOptions.IgnoreInfinity;
 
-        _view.NumberOfValuesOriginal = _doc.NumberOfValuesOriginal;
-        _view.NumberOfValuesFiltered = _doc.NumberOfValuesFiltered;
-        _view.NumberOfNaNValues = _doc.NumberOfNaNValues;
-        _view.NumberOfInfiniteValues = _doc.NumberOfInfiniteValues;
-        _view.MinimumValue = _doc.MinimumValue;
-        _view.MaximumValue = _doc.MaximumValue;
-
-        _view.IgnoreNaNValues = _doc.CreationOptions.IgnoreNaN;
-        _view.IgnoreInfiniteValues = _doc.CreationOptions.IgnoreInfinity;
-
-        _view.IgnoreValuesBelowLowerBoundary = _doc.CreationOptions.LowerBoundaryToIgnore.HasValue;
-        _view.IsLowerBoundaryInclusive = _doc.CreationOptions.IsLowerBoundaryInclusive;
+        IgnoreValuesBelowLowerBoundary = _doc.CreationOptions.LowerBoundaryToIgnore.HasValue;
+        IsLowerBoundaryInclusive = _doc.CreationOptions.IsLowerBoundaryInclusive;
         if (_doc.CreationOptions.LowerBoundaryToIgnore.HasValue)
-          _view.LowerBoundary = _doc.CreationOptions.LowerBoundaryToIgnore.Value;
+          LowerBoundary = _doc.CreationOptions.LowerBoundaryToIgnore.Value;
 
-        _view.IgnoreValuesAboveUpperBoundary = _doc.CreationOptions.UpperBoundaryToIgnore.HasValue;
-        _view.IsUpperBoundaryInclusive = _doc.CreationOptions.IsUpperBoundaryInclusive;
+        IgnoreValuesAboveUpperBoundary = _doc.CreationOptions.UpperBoundaryToIgnore.HasValue;
+        IsUpperBoundaryInclusive = _doc.CreationOptions.IsUpperBoundaryInclusive;
         if (_doc.CreationOptions.UpperBoundaryToIgnore.HasValue)
-          _view.UpperBoundary = _doc.CreationOptions.UpperBoundaryToIgnore.Value;
-
-        _view.BinningType = _binningTypes;
-
-        _view.UseAutomaticBinning = !_doc.CreationOptions.IsUserDefinedBinningType;
-
-        _view.BinningView = _binningController.ViewObject;
+          UpperBoundary = _doc.CreationOptions.UpperBoundaryToIgnore.Value;
       }
     }
 
     public override bool Apply(bool disposeController)
     {
-      _doc.CreationOptions.IgnoreNaN = _view.IgnoreNaNValues;
-      _doc.CreationOptions.IgnoreInfinity = _view.IgnoreInfiniteValues;
+      _doc.CreationOptions.IgnoreNaN = IgnoreNaNValues;
+      _doc.CreationOptions.IgnoreInfinity = IgnoreInfiniteValues;
 
-      if (_view.IgnoreValuesBelowLowerBoundary)
+      if (IgnoreValuesBelowLowerBoundary)
       {
-        _doc.CreationOptions.IsLowerBoundaryInclusive = _view.IsLowerBoundaryInclusive;
-        _doc.CreationOptions.LowerBoundaryToIgnore = _view.LowerBoundary;
+        _doc.CreationOptions.IsLowerBoundaryInclusive = IsLowerBoundaryInclusive;
+        _doc.CreationOptions.LowerBoundaryToIgnore = LowerBoundary;
       }
       else
       {
@@ -161,10 +379,10 @@ namespace Altaxo.Gui.Analysis.Statistics
         _doc.CreationOptions.LowerBoundaryToIgnore = null;
       }
 
-      if (_view.IgnoreValuesAboveUpperBoundary)
+      if (IgnoreValuesAboveUpperBoundary)
       {
-        _doc.CreationOptions.IsUpperBoundaryInclusive = _view.IsUpperBoundaryInclusive;
-        _doc.CreationOptions.UpperBoundaryToIgnore = _view.UpperBoundary;
+        _doc.CreationOptions.IsUpperBoundaryInclusive = IsUpperBoundaryInclusive;
+        _doc.CreationOptions.UpperBoundaryToIgnore = UpperBoundary;
       }
       else
       {
@@ -172,7 +390,7 @@ namespace Altaxo.Gui.Analysis.Statistics
         _doc.CreationOptions.UpperBoundaryToIgnore = null;
       }
 
-      _doc.CreationOptions.IsUserDefinedBinningType = !_view.UseAutomaticBinning;
+      _doc.CreationOptions.IsUserDefinedBinningType = !UseAutomaticBinning;
 
       if (!_binningController.Apply(disposeController))
         return ApplyEnd(false, disposeController);
@@ -207,42 +425,29 @@ namespace Altaxo.Gui.Analysis.Statistics
       }
     }
 
-    protected override void AttachView()
+
+
+    private void EhBinningTypeChanged(Type bintype)
     {
-      base.AttachView();
-      _view.BinningTypeChanged += EhBinningTypeChanged;
-      _view.AutomaticBinningTypeChanged += EhAutomaticBinningTypeChanged;
-    }
+      if (bintype is not null)
+      {
 
-    protected override void DetachView()
-    {
-      _view.AutomaticBinningTypeChanged -= EhAutomaticBinningTypeChanged;
-      _view.BinningTypeChanged -= EhBinningTypeChanged;
-      base.DetachView();
-    }
+        if (_doc.CreationOptions.Binning.GetType() == bintype)
+          return;
 
-    private void EhBinningTypeChanged()
-    {
-      var selNode = _binningTypes.FirstSelectedNode;
-      if (selNode is null)
-        return;
-      var bintype = (Type)selNode.Tag;
+        var binning = (IBinning)Activator.CreateInstance(bintype);
 
-      if (_doc.CreationOptions.Binning.GetType() == bintype)
-        return;
+        _doc.CreationOptions.Binning = binning;
 
-      var binning = (IBinning)Activator.CreateInstance(bintype);
-
-      _doc.CreationOptions.Binning = binning;
-
-      HistogramCreation.PopulateHistogramCreationInformation(_doc);
-      Initialize(true);
+        HistogramCreation.PopulateHistogramCreationInformation(_doc);
+        Initialize(true);
+      }
     }
 
     private void EhAutomaticBinningTypeChanged()
     {
       var wasUserBefore = _doc.CreationOptions.IsUserDefinedBinningType;
-      _doc.CreationOptions.IsUserDefinedBinningType = !_view.UseAutomaticBinning;
+      _doc.CreationOptions.IsUserDefinedBinningType = !UseAutomaticBinning;
 
       if (!_doc.CreationOptions.IsUserDefinedBinningType && wasUserBefore)
       {
