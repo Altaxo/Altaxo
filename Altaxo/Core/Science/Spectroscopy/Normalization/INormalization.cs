2d0e3aae6dd61f0d5ed600ba4a407b5a1edd3b85
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-05-05 23:27:05 +0200

    Add  min-max normalization for arrays

---
+++ Altaxo/Core/Science/Spectroscopy/Normalization/INormalization.cs
@@ -0,0 +1,40 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+
+namespace Altaxo.Science.Spectroscopy.Normalization
+{
+  /// <summary>
+  /// Interface to normalization methods.
+  /// </summary>
+  public interface INormalization
+  {
+    /// <summary>
+    /// Executes the normalization algorithm.
+    /// </summary>
+    /// <param name="input">The input array.</param>
+    /// <returns>The normalized array.</returns>
+    double[] Execute(double[] input);
+  }
+}
---
+++ Altaxo/Core/Science/Spectroscopy/Normalization/MinMaxNormalization.cs
@@ -0,0 +1,44 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System.Linq;
+
+namespace Altaxo.Science.Spectroscopy.Normalization
+{
+  /// <summary>
+  /// Executes min-max normalization : y' = (y-min)/(max-min), in which min and max are the minimal and maximal values of the array.
+  /// </summary>
+  /// <seealso cref="Altaxo.Science.Spectroscopy.Normalization.INormalization" />
+  public class MinMaxNormalization : INormalization
+  {
+    /// <inheritdoc/>
+    public double[] Execute(double[] x)
+    {
+      var min = x.Min();
+      var max = x.Max();
+      var delta = max - min;
+      return x.Select(yy => (yy - min) / delta).ToArray();
+    }
+  }
+}
