b574bba61b76e9ec3d5d785becd55eac1f1eefd3
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-06-02 22:01:20 +0200

    Add PearsonVII fit function (area error calculation still missing)

---
+++ Altaxo/Core/Calc/FitFunctions/Peaks/PearsonVII.cs
@@ -0,0 +1,329 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+#nullable enable
+using System;
+using Altaxo.Calc.FitFunctions.Peaks;
+using Altaxo.Calc.LinearAlgebra;
+using Altaxo.Calc.Regression.Nonlinear;
+using Altaxo.Main;
+
+namespace Altaxo.Calc.FitFunctions.Peaks
+{
+  /// <summary>
+  /// Fit fuction with one or more PearsonVII shaped peaks, with a background polynomial
+  /// of variable order.
+  /// </summary>
+  [FitFunctionClass]
+  public class PearsonVII : IFitFunction, IFitFunctionPeak, IImmutable
+  {
+    const string ParameterBaseName0 = "a";
+    const string ParameterBaseName1 = "xc";
+    const string ParameterBaseName2 = "w";
+    const string ParameterBaseName3 = "m";
+
+
+    /// <summary>The order of the background polynomial.</summary>
+    private readonly int _orderOfBackgroundPolynomial;
+    /// <summary>The order of the polynomial with negative exponents.</summary>
+    private readonly int _numberOfTerms;
+
+    #region Serialization
+
+    /// <summary>
+    /// 2022-06-01 Initial version
+    /// </summary>
+    [Altaxo.Serialization.Xml.XmlSerializationSurrogateFor(typeof(PearsonVII), 0)]
+    private class XmlSerializationSurrogate0 : Altaxo.Serialization.Xml.IXmlSerializationSurrogate
+    {
+      public virtual void Serialize(object obj, Altaxo.Serialization.Xml.IXmlSerializationInfo info)
+      {
+        var s = (PearsonVII)obj;
+        info.AddValue("NumberOfTerms", s._numberOfTerms);
+        info.AddValue("OrderOfBackgroundPolynomial", s._orderOfBackgroundPolynomial);
+      }
+
+      public virtual object Deserialize(object? o, Altaxo.Serialization.Xml.IXmlDeserializationInfo info, object? parent)
+      {
+        var numberOfTerms = info.GetInt32("NumberOfTerms");
+        var orderOfBackgroundPolynomial = info.GetInt32("OrderOfBackgroundPolynomial");
+        return new PearsonVII(numberOfTerms, orderOfBackgroundPolynomial);
+      }
+    }
+
+    #endregion Serialization
+
+    public PearsonVII()
+    {
+      _numberOfTerms = 1;
+      _orderOfBackgroundPolynomial = -1;
+    }
+
+    public PearsonVII(int numberOfTerms, int orderOfBackgroundPolynomial)
+    {
+      _numberOfTerms = numberOfTerms;
+      _orderOfBackgroundPolynomial = orderOfBackgroundPolynomial;
+
+      if (!(_orderOfBackgroundPolynomial >= -1))
+        throw new ArgumentOutOfRangeException("Order of background polynomial has to be greater than or equal to zero, or -1 in order to deactivate it.");
+      if (!(_numberOfTerms >= 1))
+        throw new ArgumentOutOfRangeException("Number of terms has to be greater than or equal to 1");
+
+    }
+
+    [FitFunctionCreator("PearsonVII", "Peaks", 1, 1, 4)]
+    [System.ComponentModel.Description("${res:Altaxo.Calc.FitFunctions.Peaks.PearsonVII}")]
+    public static IFitFunction Create_1_M1()
+    {
+      return new PearsonVII(1, -1);
+    }
+
+    /// <summary>
+    /// Gets the order of the background polynomial.
+    /// </summary>
+    public int OrderOfBackgroundPolynomial
+    {
+      get { return _orderOfBackgroundPolynomial; }
+      /*
+      init {
+        if (!(_orderOfBackgroundPolynomial >= -1))
+          throw new ArgumentOutOfRangeException("Order of background polynomial must either be -1 (to disable it) or >=0", nameof(OrderOfBackgroundPolynomial));
+        _orderOfBackgroundPolynomial = value;
+      }
+      */
+    }
+
+
+
+    /// <summary>
+    /// Creates a new instance with the provided order of the background polynomial.
+    /// </summary>
+    /// <param name="orderOfBackgroundPolynomial">The order of the background polynomial. If set to -1, the background polynomial will be disabled.</param>
+    /// <returns>New instance with the background polynomial of the provided order.</returns>
+    public PearsonVII WithOrderOfBackgroundPolynomial(int orderOfBackgroundPolynomial)
+    {
+      if (!(orderOfBackgroundPolynomial >= -1))
+        throw new ArgumentOutOfRangeException($"{nameof(orderOfBackgroundPolynomial)} must be greater than or equal to 0, or -1 in order to deactivate it.");
+
+      if (!(_orderOfBackgroundPolynomial == orderOfBackgroundPolynomial))
+      {
+        return new PearsonVII(_numberOfTerms, orderOfBackgroundPolynomial);
+      }
+      else
+      {
+        return this;
+      }
+    }
+
+    /// <summary>
+    /// Gets the number of Voigt terms.
+    /// </summary>
+    public int NumberOfTerms => _numberOfTerms;
+
+    /// <summary>
+    /// Creates a new instance with the provided number of Lorentzian (Cauchy) terms.
+    /// </summary>
+    /// <param name="numberOfTerms">The number of Lorentzian (Cauchy) terms (should be greater than or equal to 1).</param>
+    /// <returns>New instance with the provided number of Lorentzian (Cauchy) terms.</returns>
+    public PearsonVII WithNumberOfTerms(int numberOfTerms)
+    {
+      if (!(numberOfTerms >= 1))
+        throw new ArgumentOutOfRangeException($"{nameof(numberOfTerms)} must be greater than or equal to 1");
+
+      if (!(_numberOfTerms == numberOfTerms))
+      {
+        return new PearsonVII(numberOfTerms, _orderOfBackgroundPolynomial);
+      }
+      else
+      {
+        return this;
+      }
+    }
+
+    #region IFitFunction Members
+
+    public int NumberOfIndependentVariables
+    {
+      get
+      {
+        return 1;
+      }
+    }
+
+    public int NumberOfDependentVariables
+    {
+      get
+      {
+        return 1;
+      }
+    }
+
+    public int NumberOfParameters
+    {
+      get
+      {
+        return _numberOfTerms * 4 + _orderOfBackgroundPolynomial + 1;
+      }
+    }
+
+    public string IndependentVariableName(int i)
+    {
+      return "x";
+    }
+
+    public string DependentVariableName(int i)
+    {
+      return "y";
+    }
+
+    public string ParameterName(int i)
+    {
+      int k = i - 4 * _numberOfTerms;
+      if (k < 0)
+      {
+        int j = i / 4;
+        return (i % 4) switch
+        {
+          0 => FormattableString.Invariant($"{ParameterBaseName0}{j}"),
+          1 => FormattableString.Invariant($"{ParameterBaseName1}{j}"),
+          2 => FormattableString.Invariant($"{ParameterBaseName2}{j}"),
+          3 => FormattableString.Invariant($"{ParameterBaseName3}{j}"),
+          _ => throw new InvalidProgramException()
+        };
+      }
+      else
+      {
+        return FormattableString.Invariant($"b{k}");
+      }
+    }
+
+    public double DefaultParameterValue(int i)
+    {
+      int k = i - 4 * _numberOfTerms;
+      if (k < 0 && i % 4 == 2)
+        return 1;
+      else
+        return 0;
+    }
+
+    public IVarianceScaling? DefaultVarianceScaling(int i)
+    {
+      return null;
+    }
+
+    public void Evaluate(double[] X, double[] P, double[] Y)
+    {
+      // evaluation of gaussian terms
+      double sumTerms = 0, sumPolynomial = 0;
+      for (int i = 0, j = 0; i < _numberOfTerms; ++i, j += 4)
+      {
+        double x = (X[0] - P[j + 1]) / P[j + 2];
+        sumTerms += P[j] * Math.Pow(1 + (Math.Pow(2, 1 / P[j + 3]) - 1) * RMath.Pow2(x), -P[j + 3]);
+      }
+
+      if (_orderOfBackgroundPolynomial >= 0)
+      {
+        int offset = 4 * _numberOfTerms;
+        // evaluation of terms x^0 .. x^n
+        sumPolynomial = P[_orderOfBackgroundPolynomial + offset];
+        for (int i = _orderOfBackgroundPolynomial - 1; i >= 0; i--)
+        {
+          sumPolynomial *= X[0];
+          sumPolynomial += P[i + offset];
+        }
+      }
+      Y[0] = sumTerms + sumPolynomial;
+    }
+
+    /// <summary>
+    /// Not functional because instance is immutable.
+    /// </summary>
+    public event EventHandler? Changed { add { } remove { } }
+
+    #endregion IFitFunction Members
+
+
+
+    /// <inheritdoc/>
+    public double[] GetInitialParametersFromHeightPositionAndWidthAtRelativeHeight(double height, double position, double width, double relativeHeight)
+    {
+      if (!(relativeHeight > 0 && relativeHeight < 1))
+        throw new ArgumentException("RelativeHeight should be in the open interval (0,1)", nameof(relativeHeight));
+
+      var w = Math.Abs(0.5* width * Math.Sqrt(relativeHeight/(1-relativeHeight)));
+      return new double[4] { height, position, w, 1 }; // Parameters for the Lorentz limit
+    }
+
+    /// <inheritdoc/>
+    IFitFunctionPeak IFitFunctionPeak.WithNumberOfTerms(int numberOfTerms)
+    {
+      return new PearsonVII(numberOfTerms, this.OrderOfBackgroundPolynomial);
+    }
+
+    /// <inheritdoc/>
+    public string[] ParameterNamesForOnePeak => new string[] { ParameterBaseName0, ParameterBaseName1, ParameterBaseName2, ParameterBaseName3 };
+
+    /// <inheritdoc/>
+    public (double Position, double Area, double Height, double FWHM) GetPositionAreaHeightFWHMFromSinglePeakParameters(double[] parameters)
+    {
+      if (parameters is null || parameters.Length != 4)
+        throw new ArgumentException(nameof(parameters));
+
+      var area = parameters[0];
+      var pos = parameters[1];
+      var height = parameters[0] * Altaxo.Calc.ComplexErrorFunctionRelated.Voigt(0, parameters[2], parameters[3]);
+      var fwhm = 2 * Altaxo.Calc.ComplexErrorFunctionRelated.VoigtHalfWidthHalfMaximum(parameters[2], parameters[3]);
+
+      return (pos, area, height, fwhm);
+    }
+
+    static double SafeSqrt(double x) => Math.Sqrt(Math.Max(0, x));
+
+    public (double Position, double PositionVariance, double Area, double AreaVariance, double Height, double HeightVariance, double FWHM, double FWHMVariance)
+      GetPositionAreaHeightFwhmFromSinglePeakParameters(double[] parameters, IROMatrix<double> cv)
+    {
+      if (parameters is null || parameters.Length != 4)
+        throw new ArgumentException(nameof(parameters));
+
+
+      var height = parameters[0];
+      var pos = parameters[1];
+      var w = Math.Abs(parameters[2]);
+      var m = parameters[3];
+
+      var heightVariance = cv is null ? 0 : Math.Sqrt(cv[0, 0]);
+      var posVariance = cv is null ? 0 : Math.Sqrt(cv[1, 1]);
+
+      double fwhm=2*w;
+      double fwhmVariance = cv is null ? 0 : 2 * Math.Sqrt(cv[2,2]);
+
+      double area = height * w * Math.Sqrt(Math.PI/(Math.Pow(2, 1/m)-1)) * GammaRelated.Gamma(m-0.5)/GammaRelated.Gamma(m);
+      double areaVariance = 0;
+
+      return (pos, posVariance, area, areaVariance, height, heightVariance, fwhm, fwhmVariance);
+    }
+  }
+}
+
+
