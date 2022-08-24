5e2a18184c6b14bc7eb1b036d8d06b6258c185b2
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-08-24 23:43:00 +0200

    Add resampling by interpolation

---
+++ Altaxo/Core/Calc/LinearAlgebra/LinearlySpacedIntervalByStartEndStep.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Altaxo.Main;
+using Altaxo.Science.Spectroscopy.Cropping;
+
+namespace Altaxo.Calc.LinearAlgebra
+{
+  /// <summary>
+  /// Defines a linear spaced closed interval defined by start, end and step.
+  /// </summary>
+  public record LinearlySpacedIntervalByStartEndStep : IImmutable, IROVector<double>
+  {
+    /// <summary>
+    /// Start of the interval (inclusive).
+    /// </summary>
+    public double Start { get; }
+
+    /// <summary>
+    /// Start of the interval (inclusive if step can divide the interval by an integer number).
+    /// </summary>
+    public double End { get; }
+
+    /// <summary>
+    /// Gets the step size.
+    /// </summary>
+    public double Step { get; }
+
+    /// <summary>The number of elements.</summary>
+    public int Length { get; }
+
+    /// <summary>The number of elements.</summary>
+    public int Count => Length;
+
+    #region Serialization
+
+    [Altaxo.Serialization.Xml.XmlSerializationSurrogateFor(typeof(LinearlySpacedIntervalByStartEndStep), 0)]
+    public class SerializationSurrogate0 : Altaxo.Serialization.Xml.IXmlSerializationSurrogate
+    {
+      public void Serialize(object obj, Altaxo.Serialization.Xml.IXmlSerializationInfo info)
+      {
+        var s = (LinearlySpacedIntervalByStartEndStep)obj;
+        info.AddValue("Start", s.Start);
+        info.AddValue("End", s.End);
+        info.AddValue("Step", s.Step);
+      }
+
+      public object Deserialize(object? o, Altaxo.Serialization.Xml.IXmlDeserializationInfo info, object? parent)
+      {
+        var start = info.GetDouble("Start");
+        var end = info.GetDouble("End");
+        var step = info.GetDouble("Step");
+
+        return new LinearlySpacedIntervalByStartEndStep(start, end, step);
+      }
+    }
+    #endregion
+
+
+    public LinearlySpacedIntervalByStartEndStep(double start, double end, double step)
+    {
+      if (!(Math.Sign(end - start) == Math.Sign(step)))
+        throw new ArgumentException($"Sign of step={step} does not match sign of (end-start)={end - start}");
+
+      Start = start;
+      End = end;
+      Step = step;
+
+      if (End - Start == 0)
+      {
+        Length = 1;
+      }
+      else
+      {
+        var r = (End - Start) / Step;
+        var ru = Math.Ceiling(r);
+        Length = Math.Abs((ru - r) / ru) < 1E-6 ? Math.Max(0, (int)(ru + 1)) : Math.Max(0, (int)ru);
+      }
+    }
+
+
+
+    /// <summary>
+    /// Gets the element at the specified index.
+    /// </summary>
+    /// <returns>Element at the specified index.</returns>
+    public double this[int index] => Start + index * Step;
+
+    /// <inheritdoc/>
+    public IEnumerator<double> GetEnumerator()
+    {
+      for (var i = 0; i < Length; ++i)
+      {
+        yield return this[i];
+      }
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      for (var i = 0; i < Length; ++i)
+      {
+        yield return this[i];
+      }
+    }
+  }
+
+  // TODO
+  // By:
+  // StartEndCount,
+  // StartCountInterval,
+  // EndCountInterval
+
+
+
+}
---
+++ Altaxo/Core/Science/Spectroscopy/Resampling/ResamplingByInterpolation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Altaxo.Calc.Interpolation;
+using Altaxo.Calc.LinearAlgebra;
+using Altaxo.Calc.Regression.Nonlinear;
+using Altaxo.Main;
+using Altaxo.Science.Spectroscopy.Cropping;
+
+namespace Altaxo.Science.Spectroscopy.Resampling
+{
+  /// <summary>
+  /// Resamples a function by using an interpolation function, and sampling points at which the interpolation function is evaluated.
+  /// </summary>
+  public record ResamplingByInterpolation : IResampling, ICropping, IImmutable
+  {
+    /// <summary>
+    /// Gets the interpolation function to be used.
+    /// </summary>
+    public IInterpolationFunctionOptions Interpolation { get; init; } = new AkimaCubicSplineOptions();
+
+    /// <summary>
+    /// Gets the sampling points.
+    /// </summary>
+    IROVector<double> SamplingPoints { get; init; } = new LinearlySpacedIntervalByStartEndStep(0, 1, 1);
+
+    #region Serialization
+
+    [Altaxo.Serialization.Xml.XmlSerializationSurrogateFor(typeof(ResamplingByInterpolation), 0)]
+    public class SerializationSurrogate0 : Altaxo.Serialization.Xml.IXmlSerializationSurrogate
+    {
+      public void Serialize(object obj, Altaxo.Serialization.Xml.IXmlSerializationInfo info)
+      {
+        var s = (ResamplingByInterpolation)obj;
+        info.AddValue("Interpolation", s.Interpolation);
+        info.AddValue("SamplingPoints", s.SamplingPoints);
+      }
+
+      public object Deserialize(object? o, Altaxo.Serialization.Xml.IXmlDeserializationInfo info, object? parent)
+      {
+        var interpolation = info.GetValue<IInterpolationFunctionOptions>("Interpolation",null);
+        var samplingPoints = info.GetValue<IROVector<double>>("SamplingPoints",null);
+        return new ResamplingByInterpolation() { Interpolation = interpolation, SamplingPoints = samplingPoints };
+      }
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public (double[] x, double[] y, int[]? regions) Execute(double[] x, double[] y, int[]? regions)
+    {
+      var spline = Interpolation.Interpolate(x, y);
+
+      var xx = new double[SamplingPoints.Length];
+      var yy = new double[SamplingPoints.Length];
+
+      for(int i=0;i<xx.Length;++i)
+      {
+        xx[i] = SamplingPoints[i];
+        yy[i] = spline.GetYOfX(xx[i]);
+      }
+
+      return (xx, yy, null);
+    }
+  }
+}
