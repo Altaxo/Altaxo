b2190e014b181895b6abe8c0792915e983384a41
Dirk Lellinger <dlellinger@users.sourceforge.net>
2022-09-23 23:38:55 +0200

    Add pseudo Voigt fit function

---
+++ Altaxo/Core/Calc/FitFunctions/Peaks/PseudoVoigtAmplitude.cs
@@ -0,0 +1,423 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2022 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+#nullable enable
+using System;
+using System.Collections.Generic;
+using Altaxo.Calc.LinearAlgebra;
+using Altaxo.Calc.Regression.Nonlinear;
+using Altaxo.Main;
+
+namespace Altaxo.Calc.FitFunctions.Peaks
+{
+  /// <summary>
+  /// Fit function with a blend of a Gaussian and a Cauchy (Lorentz) element, see <see href="https://de.wikipedia.org/wiki/Voigt-Profil"/>.
+  /// The blend factor nu has a range of [0, 1].
+  /// </summary>
+  [FitFunctionClass]
+  public class PseudoVoigtAmplitude : IFitFunction, IFitFunctionWithGradient, IFitFunctionPeak, IImmutable
+  {
+    private const string ParameterBaseName0 = "a";
+    private const string ParameterBaseName1 = "xc";
+    private const string ParameterBaseName2 = "w";
+    private const string ParameterBaseName3 = "nu";
+    const int NumberOfParametersPerPeak = 4;
+    const double Log2 = 0.69314718055994530941723212145818;
+    const double SqrtPiByLog2 = 2.1289340388624523586305351924692;
+
+    /// <summary>The order of the background polynomial.</summary>
+    private readonly int _orderOfBackgroundPolynomial;
+    /// <summary>The order of the polynomial with negative exponents.</summary>
+    private readonly int _numberOfTerms;
+
+    #region Serialization
+
+    /// <summary>
+    /// 2022-09-23 Initial version
+    /// </summary>
+    [Altaxo.Serialization.Xml.XmlSerializationSurrogateFor(typeof(PseudoVoigtAmplitude), 0)]
+    private class XmlSerializationSurrogate0 : Altaxo.Serialization.Xml.IXmlSerializationSurrogate
+    {
+      public virtual void Serialize(object obj, Altaxo.Serialization.Xml.IXmlSerializationInfo info)
+      {
+        var s = (PseudoVoigtAmplitude)obj;
+        info.AddValue("NumberOfTerms", s._numberOfTerms);
+        info.AddValue("OrderOfBackgroundPolynomial", s._orderOfBackgroundPolynomial);
+      }
+
+      public virtual object Deserialize(object? o, Altaxo.Serialization.Xml.IXmlDeserializationInfo info, object? parent)
+      {
+        var numberOfTerms = info.GetInt32("NumberOfTerms");
+        var orderOfBackgroundPolynomial = info.GetInt32("OrderOfBackgroundPolynomial");
+        return new PseudoVoigtAmplitude(numberOfTerms, orderOfBackgroundPolynomial);
+      }
+    }
+
+    #endregion Serialization
+
+    public PseudoVoigtAmplitude()
+    {
+      _numberOfTerms = 1;
+      _orderOfBackgroundPolynomial = -1;
+    }
+
+    public PseudoVoigtAmplitude(int numberOfTerms, int orderOfBackgroundPolynomial)
+    {
+      _numberOfTerms = numberOfTerms;
+      _orderOfBackgroundPolynomial = orderOfBackgroundPolynomial;
+
+      if (!(_orderOfBackgroundPolynomial >= -1))
+        throw new ArgumentOutOfRangeException("Order of background polynomial has to be greater than or equal to zero, or -1 in order to deactivate it.");
+      if (!(_numberOfTerms >= 1))
+        throw new ArgumentOutOfRangeException("Number of terms has to be greater than or equal to 1");
+
+    }
+
+    [FitFunctionCreator("PseudoVoigtAmplitude", "Peaks", 1, 1, NumberOfParametersPerPeak)]
+    [System.ComponentModel.Description("${res:Altaxo.Calc.FitFunctions.Peaks.PseudoVoigtAmplitude}")]
+    public static IFitFunction Create_1_M1()
+    {
+      return new PseudoVoigtAmplitude(1, -1);
+    }
+
+    /// <summary>
+    /// Gets the order of the background polynomial.
+    /// </summary>
+    public int OrderOfBackgroundPolynomial
+    {
+      get { return _orderOfBackgroundPolynomial; }
+      /*
+      init {
+        if (!(_orderOfBackgroundPolynomial >= -1))
+          throw new ArgumentOutOfRangeException("Order of background polynomial must either be -1 (to disable it) or >=0", nameof(OrderOfBackgroundPolynomial));
+        _orderOfBackgroundPolynomial = value;
+      }
+      */
+    }
+
+
+
+    /// <summary>
+    /// Creates a new instance with the provided order of the background polynomial.
+    /// </summary>
+    /// <param name="orderOfBackgroundPolynomial">The order of the background polynomial. If set to -1, the background polynomial will be disabled.</param>
+    /// <returns>New instance with the background polynomial of the provided order.</returns>
+    public PseudoVoigtAmplitude WithOrderOfBackgroundPolynomial(int orderOfBackgroundPolynomial)
+    {
+      if (!(orderOfBackgroundPolynomial >= -1))
+        throw new ArgumentOutOfRangeException($"{nameof(orderOfBackgroundPolynomial)} must be greater than or equal to 0, or -1 in order to deactivate it.");
+
+      if (!(_orderOfBackgroundPolynomial == orderOfBackgroundPolynomial))
+      {
+        return new PseudoVoigtAmplitude(_numberOfTerms, orderOfBackgroundPolynomial);
+      }
+      else
+      {
+        return this;
+      }
+    }
+
+    /// <summary>
+    /// Gets the number of Voigt terms.
+    /// </summary>
+    public int NumberOfTerms => _numberOfTerms;
+
+    /// <summary>
+    /// Creates a new instance with the provided number of Lorentzian (Cauchy) terms.
+    /// </summary>
+    /// <param name="numberOfTerms">The number of Lorentzian (Cauchy) terms (should be greater than or equal to 1).</param>
+    /// <returns>New instance with the provided number of Lorentzian (Cauchy) terms.</returns>
+    public PseudoVoigtAmplitude WithNumberOfTerms(int numberOfTerms)
+    {
+      if (!(numberOfTerms >= 1))
+        throw new ArgumentOutOfRangeException($"{nameof(numberOfTerms)} must be greater than or equal to 1");
+
+      if (!(_numberOfTerms == numberOfTerms))
+      {
+        return new PseudoVoigtAmplitude(numberOfTerms, _orderOfBackgroundPolynomial);
+      }
+      else
+      {
+        return this;
+      }
+    }
+
+    #region IFitFunction Members
+
+    public int NumberOfIndependentVariables
+    {
+      get
+      {
+        return 1;
+      }
+    }
+
+    public int NumberOfDependentVariables
+    {
+      get
+      {
+        return 1;
+      }
+    }
+
+    public int NumberOfParameters
+    {
+      get
+      {
+        return _numberOfTerms * NumberOfParametersPerPeak + _orderOfBackgroundPolynomial + 1;
+      }
+    }
+
+    public string IndependentVariableName(int i)
+    {
+      return "x";
+    }
+
+    public string DependentVariableName(int i)
+    {
+      return "y";
+    }
+
+    public string ParameterName(int i)
+    {
+      int k = i - NumberOfParametersPerPeak * _numberOfTerms;
+      if (k < 0)
+      {
+        int j = i / NumberOfParametersPerPeak;
+        return (i % NumberOfParametersPerPeak) switch
+        {
+          0 => FormattableString.Invariant($"{ParameterBaseName0}{j}"),
+          1 => FormattableString.Invariant($"{ParameterBaseName1}{j}"),
+          2 => FormattableString.Invariant($"{ParameterBaseName2}{j}"),
+          3 => FormattableString.Invariant($"{ParameterBaseName3}{j}"),
+          _ => throw new InvalidProgramException()
+        };
+      }
+      else
+      {
+        return FormattableString.Invariant($"b{k}");
+      }
+    }
+
+    public double DefaultParameterValue(int i)
+    {
+      int k = i - NumberOfParametersPerPeak * _numberOfTerms;
+      if (k < 0)
+      {
+        return (i % NumberOfParametersPerPeak) switch
+        {
+          0 => 1, // amplitude
+          1 => 0, // position
+          2 => 1, // width
+          3 => 0, // nu (Gaussian),
+          _ => 0
+        };
+      }
+      else
+      {
+        return 0; // no background
+      }
+    }
+    public IVarianceScaling? DefaultVarianceScaling(int i)
+    {
+      return null;
+    }
+
+    public void Evaluate(double[] X, double[] P, double[] Y)
+    {
+      // evaluation of gaussian terms
+      double sumTerms = 0, sumPolynomial = 0;
+      for (int i = 0, j = 0; i < _numberOfTerms; ++i, j += NumberOfParametersPerPeak)
+      {
+        double arg = (X[0] - P[j + 1]) / P[j + 2];
+        sumTerms += P[j] * ((P[j + 3]) / (1 + arg * arg) + (1 - P[j + 3]) * Math.Exp(-Log2 * arg * arg));
+      }
+
+      if (_orderOfBackgroundPolynomial >= 0)
+      {
+        int offset = NumberOfParametersPerPeak * _numberOfTerms;
+        // evaluation of terms x^0 .. x^n
+        sumPolynomial = P[_orderOfBackgroundPolynomial + offset];
+        for (int i = _orderOfBackgroundPolynomial - 1; i >= 0; i--)
+        {
+          sumPolynomial *= X[0];
+          sumPolynomial += P[i + offset];
+        }
+      }
+      Y[0] = sumTerms + sumPolynomial;
+    }
+
+    public void EvaluateMultiple(IROMatrix<double> independent, IReadOnlyList<double> P, IReadOnlyList<bool>? independentVariableChoice, IVector<double> FV)
+    {
+      for (int r = 0; r < independent.RowCount; ++r)
+      {
+        var x = independent[r, 0];
+        // evaluation of gaussian terms
+        double sumTerms = 0, sumPolynomial = 0;
+        for (int i = 0, j = 0; i < _numberOfTerms; ++i, j += NumberOfParametersPerPeak)
+        {
+          double arg = (x - P[j + 1]) / P[j + 2];
+          sumTerms += P[j] * ((P[j + 3]) / (1 + arg * arg) + (1 - P[j + 3]) * Math.Exp(-Log2 * arg * arg));
+        }
+
+        if (_orderOfBackgroundPolynomial >= 0)
+        {
+          int offset = NumberOfParametersPerPeak * _numberOfTerms;
+          // evaluation of terms x^0 .. x^n
+          sumPolynomial = P[_orderOfBackgroundPolynomial + offset];
+          for (int i = _orderOfBackgroundPolynomial - 1; i >= 0; i--)
+          {
+            sumPolynomial *= x;
+            sumPolynomial += P[i + offset];
+          }
+        }
+
+        FV[r] = sumTerms + sumPolynomial;
+      }
+    }
+
+    public void EvaluateGradient(IROMatrix<double> X, IReadOnlyList<double> P, IReadOnlyList<bool>? independentVariableChoice, IMatrix<double> DY)
+    {
+      var rowCount = X.RowCount;
+      for (int r = 0; r < rowCount; ++r)
+      {
+        var x = X[r, 0];
+
+        // at first, the gaussian terms
+        for (int i = 0, j = 0; i < _numberOfTerms; ++i, j += NumberOfParametersPerPeak)
+        {
+          var height = P[j];
+          var w = P[j + 2];
+          var arg = (x - P[j + 1]) / w;
+          var nu = P[j + 3];
+
+          var expTerm = Math.Exp(-Log2 * arg * arg);
+          var lorentzTerm = 1 / (1 + arg * arg);
+
+          DY[r, j + 0] = nu * lorentzTerm + (1 - nu) * expTerm;
+          DY[r, j + 1] = height * (2 * arg / w) * (nu * lorentzTerm * lorentzTerm + (1 - nu) * expTerm * Log2);
+          DY[r, j + 2] = height * (2 * arg * arg / w) * (nu * lorentzTerm * lorentzTerm + (1 - nu) * expTerm * Log2);
+          DY[r, j + 3] = height * (lorentzTerm - expTerm);
+        }
+
+        if (_orderOfBackgroundPolynomial >= 0)
+        {
+          double xn = 1;
+          for (int i = 0, j = NumberOfParametersPerPeak * _numberOfTerms; i <= _orderOfBackgroundPolynomial; ++i, ++j)
+          {
+            DY[r, j] = xn;
+            xn *= x;
+          }
+        }
+      }
+    }
+
+
+    /// <summary>
+    /// Not functional because instance is immutable.
+    /// </summary>
+    public event EventHandler? Changed { add { } remove { } }
+
+    #endregion IFitFunction Members
+
+
+
+    /// <inheritdoc/>
+    public double[] GetInitialParametersFromHeightPositionAndWidthAtRelativeHeight(double height, double position, double width, double relativeHeight)
+    {
+      if (!(relativeHeight > 0 && relativeHeight < 1))
+        throw new ArgumentException("RelativeHeight should be in the open interval (0,1)", nameof(relativeHeight));
+
+      var w = Math.Abs(0.5 * width * Log2 / Math.Sqrt(-Math.Log(relativeHeight)));
+      return new double[NumberOfParametersPerPeak] { height, position, w, 0 }; // Parameters for the Gaussian limit
+    }
+
+    /// <inheritdoc/>
+    IFitFunctionPeak IFitFunctionPeak.WithNumberOfTerms(int numberOfTerms)
+    {
+      return new PseudoVoigtAmplitude(numberOfTerms, this.OrderOfBackgroundPolynomial);
+    }
+
+    /// <inheritdoc/>
+    public string[] ParameterNamesForOnePeak => new string[] { ParameterBaseName0, ParameterBaseName1, ParameterBaseName2, ParameterBaseName3 };
+
+    /// <inheritdoc/>
+    public (double Position, double Area, double Height, double FWHM) GetPositionAreaHeightFWHMFromSinglePeakParameters(double[] parameters)
+    {
+      if (parameters is null || parameters.Length != NumberOfParametersPerPeak)
+        throw new ArgumentException(nameof(parameters));
+
+      var height = parameters[0];
+      var pos = parameters[1];
+      var w = Math.Abs(parameters[2]);
+      var nu = parameters[3];
+      double fwhm = 2 * w;
+      double area = height * w * (nu * Math.PI + (1 - nu) * SqrtPiByLog2);
+      return (pos, area, height, fwhm);
+    }
+
+    private static double SafeSqrt(double x) => Math.Sqrt(Math.Max(0, x));
+
+    public (double Position, double PositionStdDev, double Area, double AreaStdDev, double Height, double HeightStdDev, double FWHM, double FWHMStdDev)
+      GetPositionAreaHeightFWHMFromSinglePeakParameters(double[] parameters, IROMatrix<double> cv)
+    {
+      if (parameters is null || parameters.Length != NumberOfParametersPerPeak)
+        throw new ArgumentException(nameof(parameters));
+
+
+      var height = parameters[0];
+      var pos = parameters[1];
+      var w = Math.Abs(parameters[2]);
+      var nu = parameters[3];
+
+      var heightStdDev = cv is null ? 0 : Math.Sqrt(cv[0, 0]);
+      var posStdDev = cv is null ? 0 : Math.Sqrt(cv[1, 1]);
+
+      double fwhm = 2 * w;
+      double fwhmStdDev = cv is null ? 0 : 2 * Math.Sqrt(cv[2, 2]);
+
+      double area = height * w * (nu * Math.PI + (1 - nu) * SqrtPiByLog2);
+      double areaStdDev = 0;
+
+      if (cv is not null)
+      {
+        var deriv = new double[NumberOfParametersPerPeak];
+        var resVec = VectorMath.ToVector(new double[NumberOfParametersPerPeak]);
+        var mixTerm = (nu * Math.PI + (1 - nu) * SqrtPiByLog2);
+        deriv[0] = w * mixTerm;
+        deriv[1] = 0;
+        deriv[2] = height * mixTerm;
+        deriv[3] = height * w * (Math.PI - SqrtPiByLog2);
+        MatrixMath.Multiply(cv, deriv, resVec);
+        areaStdDev = SafeSqrt(VectorMath.DotProduct(deriv, resVec));
+      }
+
+      return (pos, posStdDev, area, areaStdDev, height, heightStdDev, fwhm, fwhmStdDev);
+    }
+
+
+  }
+}
+
+
