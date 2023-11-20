effc930ce8f3516298b2c050c5c8fab945199aab
Dirk Lellinger <dlellinger@users.sourceforge.net>
2023-11-20 21:55:02 +0100

    Add NormalizationMax

---
+++ Altaxo/Core/Science/Spectroscopy/Normalization/NormalizationMax.cs
@@ -0,0 +1,78 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2023 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+
+namespace Altaxo.Science.Spectroscopy.Normalization
+{
+  /// <summary>
+  /// Executes max normalization : y' = y/max, in which max is the maximum of the values of the array.
+  /// </summary>
+  /// <seealso cref="Altaxo.Science.Spectroscopy.Normalization.INormalization" />
+  public record NormalizationMax
+
+    : INormalization
+  {
+    #region Serialization
+
+    /// <summary>
+    /// 2023-11-20 V0
+    /// </summary>
+    /// <seealso cref="Altaxo.Serialization.Xml.IXmlSerializationSurrogate" />
+    [Altaxo.Serialization.Xml.XmlSerializationSurrogateFor(typeof(NormalizationMax), 0)]
+    public class SerializationSurrogate0 : Altaxo.Serialization.Xml.IXmlSerializationSurrogate
+    {
+      public void Serialize(object obj, Altaxo.Serialization.Xml.IXmlSerializationInfo info)
+      {
+      }
+
+      public object Deserialize(object? o, Altaxo.Serialization.Xml.IXmlDeserializationInfo info, object? parent)
+      {
+        return new NormalizationMax();
+      }
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public (double[] x, double[] y, int[]? regions) Execute(double[] x, double[] y, int[]? regions)
+    {
+      var yy = new double[y.Length];
+
+      foreach (var (start, end) in RegionHelper.GetRegionRanges(regions, x.Length))
+      {
+        double max = double.NegativeInfinity;
+        for (int i = start; i < end; ++i)
+        {
+          max = Math.Max(max, y[i]);
+        }
+
+        for (int i = start; i < end; ++i)
+        {
+          yy[i] = y[i] / max;
+        }
+      }
+      return (x, yy, regions);
+    }
+  }
+}
