9c7dcf87143f6005a136bf0c96fe70275472157c
Dirk Lellinger <dlellinger@users.sourceforge.net>
2024-08-19 23:34:13 +0200

    Refactoring and error fixing of the LaTeXFormulaImageStreamProvider

--- Altaxo/Dom.Presentation/Gui/Text/Viewing/LaTeXFormulaImageStreamProvider.cs
+++ Altaxo/Dom.Presentation/Gui/Text/Viewing/LaTeXFormulaImageStreamProvider.cs
@@ -26,7 +26,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using WpfMath.Parsers;
@@ -60,24 +59,26 @@ namespace Altaxo.Gui.Markdown
         return (null, null, 0, 0, 0);
       }
 
-      double cyAscent;
-      double cyDescent;
-      double cyMiddle;
+      double fontAscent;
+      double fontDescent;
+      double fontStrikethrough;
       {
         var gdiFont = new System.Drawing.Font(fontFamily, (float)fontSize, System.Drawing.FontStyle.Regular);
         int iCellSpace = gdiFont.FontFamily.GetLineSpacing(gdiFont.Style);
         int iEmHeight = gdiFont.FontFamily.GetEmHeight(gdiFont.Style);
         int iCellAscent = gdiFont.FontFamily.GetCellAscent(gdiFont.Style);
         int iCellDescent = gdiFont.FontFamily.GetCellDescent(gdiFont.Style);
-        cyAscent = fontSize * iCellAscent / iEmHeight;
-        cyDescent = fontSize * iCellDescent / iEmHeight;
-        cyMiddle = cyAscent / 3; // is only a first guess, details coming from the Wpf font
+        fontAscent = fontSize * iCellAscent / iEmHeight;
+        fontDescent = fontSize * iCellDescent / iEmHeight;
+        fontStrikethrough = fontAscent / 3; // is only a first guess, details coming from the Wpf font
         gdiFont.Dispose();
-        var ff = new FontFamily(fontFamily);
+        var ff = new System.Windows.Media.FontFamily(fontFamily);
         var tf = new Typeface(ff, System.Windows.FontStyles.Normal, System.Windows.FontWeights.Normal, System.Windows.FontStretches.Normal);
         if (tf.TryGetGlyphTypeface(out var gtf))
         {
-          cyMiddle = Math.Floor(fontSize * gtf.Height * gtf.StrikethroughPosition);
+          fontAscent = fontSize * gtf.Baseline;
+          fontDescent = fontSize * (gtf.Height - gtf.Baseline);
+          fontStrikethrough = fontSize * gtf.StrikethroughPosition;
         }
       }
 
@@ -91,10 +92,13 @@ namespace Altaxo.Gui.Markdown
         }
       }
 
+      var relativeResolution = dpiResolution / 96.0;
+      var imageWidthPixels = (int)Math.Ceiling(fontSize * box.Width * relativeResolution);
+      var imageHeightPixels = (int)(Math.Ceiling(fontSize * box.Height * relativeResolution) + Math.Ceiling(fontSize * box.Depth * relativeResolution));
       // the placement of the image depends on the depth value
-      var relativeDepth = box.Depth / box.TotalHeight;
-      var absoluteDepth = box.Depth * fontSize; // TODO: is this in 72th inch because of fontSize or in 96th inch?
-      var absoluteAscent = box.Height * fontSize;
+      var formulaDescentRelative = box.Depth / box.TotalHeight;
+      var formulaDescent = box.Depth * fontSize; // TODO: is this in 72th inch because of fontSize or in 96th inch?
+      var formulaAscent = box.Height * fontSize;
 
       double yoffset = 0;
 
@@ -105,49 +109,95 @@ namespace Altaxo.Gui.Markdown
 
       if (isIntendedForHelp1File)
       {
-        // dictionary that holds the number of additional pixels neccessary to exactly align the formula, and the value is the alignment
-        var sort = new SortedDictionary<double, (double, string)>();
+        // dictionary that holds the number vertical pixels of the image, and a tuple of the neccessary render offset and the alignment
+        var sort = new List<(int imageHeight, double yOffset_96thInch, string alignment)>();
 
-        if (relativeDepth < (1 / 16.0)) // if the formulas baseline is almost at the bottom of the image
+        if (formulaDescentRelative < (1 / 16.0)) // if the formulas baseline is almost at the bottom of the image
         {
-          sort.Add(0, (0.0, "baseline")); // then we can use baseline as vertical alight
+          sort.Add((imageHeightPixels, 0.0, "baseline")); // then we can use baseline as vertical alight
         }
-        if (absoluteAscent <= cyAscent)  // if our formula is higher than the top of the text
+        if (fontAscent >= formulaAscent)
         {
-          yoffset = cyAscent - absoluteAscent;
-          sort.Add(Math.Abs(yoffset), (yoffset, "texttop")); // then we can use texttop alignment, and we shift the formula downwards (positive offset)
+          // if our formula is not higher than the top of the text than we can use the texttop alignment
+          // texttop alignment means that the top of the formula box is aligned with the top (cyAscent) of the surrounding font
+          // we pad our formula box at the top with the difference between the font ascent and the formula ascent
+          // and we shift the formula downwards (positive offset)
+          yoffset = fontAscent - formulaAscent;
+          int additionalPixelsTop = (int)Math.Ceiling(yoffset * relativeResolution);
+          var renderOffset = yoffset;
+          sort.Add((imageHeightPixels + additionalPixelsTop, renderOffset, "texttop"));
         }
-        if (absoluteDepth <= cyDescent) // then we can use bottom alignment, and we shift the formula upwards (negative offset)
+        if (fontDescent >= formulaDescent) // then we can use bottom alignment, and we shift the formula upwards (negative offset)
         {
-          yoffset = absoluteDepth - cyDescent;
-          sort.Add(Math.Abs(yoffset), (yoffset, "bottom"));
+          // if our formula is not deeper than the bottom of the text (font) then we can use bottom alignment
+          // for a Help1 target bottom alignment means that the bottom of the formula image is aligned with the bottom (cyDescent) of the surrounding font
+          // we pad our formula box at the bottom with the difference value
+          // note that for the key we use the absolute value in order to later find the sort of padding which requires the
+          // least number of pixels to add to the formula box
+          yoffset = fontDescent - formulaDescent;
+
+          // calculate the neccessary pixels to add to the bottom
+          // by using Math.Ceiling, we shift the image a little bit to high
+          int additionalPixelsBottom = (int)Math.Ceiling(yoffset * relativeResolution);
+          // in order to compensate for this, we have to shift the formula a little bit down again
+          var renderOffset = (additionalPixelsBottom / relativeResolution) - yoffset;
+          sort.Add((imageHeightPixels + additionalPixelsBottom, renderOffset, "bottom")); // note that we store the value as negative value to mark it as bottom padding
         }
 
         {
           // Alignment: middle
+          // for a Help1 target this means that the vertical middle of the formula box is aligned with the strikethrough position of the surrounding font
           // Note that this is a moving target: we must change the vertical size of the image, but by that
           // we change the middle of the image, which changes again the offset...
-          if (isIntendedForHelp1File)
-            yoffset = absoluteDepth - absoluteAscent; // in help1 file, the baseline of text is aligned with the middle of the image
-          else
-            yoffset = 2 * cyMiddle + absoluteDepth - absoluteAscent; // if yoffset is negative, then pad at the bottom, else pad at the top
-
-          sort.Add(Math.Abs(yoffset), (yoffset, "middle"));
+          // a positive offset means we have to pad the formula at the top
+          // whereas a negative offset means we have to pad the formula box at the bottom
+          yoffset = formulaDescent - formulaAscent + 2 * fontStrikethrough;
+          int additionalPixels = (int)Math.Ceiling(Math.Abs(yoffset) * relativeResolution);
+          var imageHalfHeight = (imageHeightPixels + additionalPixels) / (2.0 * relativeResolution);
+          var renderOffset = imageHalfHeight + fontStrikethrough - formulaAscent;
+          sort.Add((imageHeightPixels + additionalPixels, renderOffset, "middle"));
         }
 
-        var firstEntry = sort.First();
-        (bmp, width96thInch, height96thInch) = RenderToBitmap(formula, _texEnvironment, fontSize, 0, firstEntry.Value.Item1, dpiResolution);
-        alignment = firstEntry.Value.Item2;
+        // now find the entry which requires the least padding by sorting by key (which is the image height)
+        // and then use the entry with the smallest image size
+        sort.Sort((x, y) => Comparer<int>.Default.Compare(x.imageHeight, y.imageHeight));
+        var firstEntry = sort[0];
+        (bmp, width96thInch, height96thInch) = RenderToBitmap(
+          formula,
+          _texEnvironment,
+          fontSize,
+          bitmapWidthPixels: imageWidthPixels,
+          bitmapHeightPixels: firstEntry.imageHeight,
+          relativeResolution: relativeResolution,
+          renderOffsetY: firstEntry.yOffset_96thInch);
+
+        alignment = firstEntry.alignment;
         yoffset = 0; // 0 as return value
       }
-      else // MAML is intended for HTML help (so we can use HTML5 alignment with pixel accuracy        )
+      else // MAML is intended for HTML help (so we can use HTML5 alignment with pixel accuracy )
       {
         alignment = "baseline";
-        var yshift = -Math.Ceiling(absoluteDepth); // we shift the formula downwards, so that the new absoluteAscent is Math.Ceiling(absoluteAscent)
-                                                   // by providing a positive offset in arg2, the image is lowered compared to the baseline
-        (bmp, width96thInch, height96thInch) = RenderToBitmap(formula, _texEnvironment, fontSize, 0, yshift, dpiResolution);
-        // bmp = WpfTeXFormulaExtensions.RenderToBitmap(formula, _texEnvironment, out box, fontSize, 0, yshift, dpiResolution);
-        yoffset = Math.Ceiling(absoluteAscent) - height96thInch; // number of pixels from image bottom to baseline (negative sign)
+        height96thInch = (int)Math.Round(imageHeightPixels / relativeResolution);
+        var yoffsetAccurate = formulaAscent - height96thInch; // number of pixels from image bottom to baseline (negative sign)
+        yoffset = Math.Ceiling(yoffsetAccurate);
+
+        if (yoffset != yoffsetAccurate)
+        {
+          imageHeightPixels += (int)Math.Ceiling(relativeResolution);
+          height96thInch = (int)Math.Round(imageHeightPixels / relativeResolution);
+          yoffsetAccurate = formulaAscent - height96thInch; // number of pixels from image bottom to baseline (negative sign)
+          yoffset = Math.Ceiling(yoffsetAccurate);
+        }
+
+
+        (bmp, width96thInch, height96thInch) = RenderToBitmap(
+          formula,
+          _texEnvironment,
+          fontSize,
+          bitmapWidthPixels: imageWidthPixels,
+          bitmapHeightPixels: imageHeightPixels,
+          relativeResolution: relativeResolution,
+          renderOffsetY: yoffset - yoffsetAccurate);
       }
 
       var fileStream = new MemoryStream();
@@ -159,41 +209,37 @@ namespace Altaxo.Gui.Markdown
       return (fileStream, alignment, (int)Math.Round(yoffset), width96thInch, height96thInch);
     }
 
-
     /// <summary>
     /// Renders a formula to a bitmap.
     /// </summary>
-    /// <param name="formulaRenderer">The formula renderer.</param>
-    /// <param name="x">The x offset of the formula.</param>
-    /// <param name="y">The y offset.
-    /// If y is negative, the distance of top to text does not change, and the distance from bottom to text changes by Ceiling(-y), because Ceiling(-y) pixel lines are added to the image.
-    /// That means, that if looked from the bottom of the image, the formula shifts upwardes, but in integer pixel steps.
-    /// If y is positive, the distance of top of image to text changes by y. Because Ceiling(y) pixel lines are added to the image,
-    /// the distance from bottom of the image to text changes by [Ceiling(y)-y], i.e. the shift is 0 for y==0, 1 for y==(0+epsilon), and 0 again for y==1.
-    /// That means, if measured from the bottom of the image, the formula shifts upwards, but maximal by 1 pixel.
-    /// </param>
-    /// <param name="dpiResolution">The resolution of the image in dpi. If not sure, use 96 dpi.</param>
+    /// <param name="formula">The formula to render.</param>
+    /// <param name="environment">The formula environment needed to render the formula.</param>
+    /// <param name="fontSize">The current font size used for rendering of the formula.</param>
+    /// <param name="bitmapWidthPixels">The width of the render bitmap in pixels.</param>
+    /// <param name="bitmapHeightPixels">The height of the render bitmap in pixels.</param>
+    /// <param name="relativeResolution">The relative resolution as defined as resolution in dpi divided by 96 dpi.</param>
+    /// <param name="renderOffsetY">The render offset in 1/96th inch. Should be positive (not tested).
+    /// Designates the distance between the top of the image and the top of the formula.
     /// <returns>The bitmap souce that represents the formula.</returns>
-    public static (BitmapSource bitmapSource, int width96thInch, int heigth96thInch) RenderToBitmap(TexFormula formula, TexEnvironment environment, double fontSize, double x, double y, double dpiResolution)
+    public static (BitmapSource bitmapSource, int width96thInch, int heigth96thInch) RenderToBitmap(
+      TexFormula formula,
+      TexEnvironment environment,
+      double fontSize,
+      int bitmapWidthPixels,
+      int bitmapHeightPixels,
+      double relativeResolution,
+      double renderOffsetY)
     {
       Box box;
       var visual = new DrawingVisual();
       using (var drawingContext = visual.RenderOpen())
       {
-        // Note that argument y in Render measures from the top. Thus is y is positive, this shifts the text down.
-        box = formula.RenderTo(drawingContext, _texEnvironment, fontSize, x, Math.Max(0, y));
+        box = formula.RenderTo(drawingContext, _texEnvironment, fontSize, 0, renderOffsetY / fontSize);
       }
 
-      var width = (int)Math.Ceiling(fontSize * box.Width); // TODO: is this in 96th inch, or because of the fontSize, in 72th inch?
-      var height = (int)(Math.Ceiling(fontSize * box.Height) + Math.Ceiling(fontSize * box.Depth));
-      //height += (int)Math.Ceiling(Math.Abs(y));
-
-      var relativeResolution = dpiResolution / 96.0;
-
-      var bitmap = new RenderTargetBitmap((int)(relativeResolution * width), (int)(relativeResolution * height), (int)(relativeResolution * 96), (int)(relativeResolution * 96), PixelFormats.Default);
+      var bitmap = new RenderTargetBitmap(bitmapWidthPixels, bitmapHeightPixels, (int)(relativeResolution * 96), (int)(relativeResolution * 96), PixelFormats.Default);
       bitmap.Render(visual);
-
-      return (bitmap, width, height);
+      return (bitmap, (int)(bitmapWidthPixels / relativeResolution), (int)(bitmapHeightPixels / relativeResolution));
     }
 
   }
