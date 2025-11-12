eb539bea73c0eca870c7d4ada7163d767a1ff036
Dirk Lellinger <dlellinger@users.sourceforge.net>
2025-11-12 20:22:59 +0100

    Improve QuickQuadraticRegression with more properties

--- Altaxo/Core/Calc/Regression/QuickQuadraticRegression.cs
+++ Altaxo/Core/Calc/Regression/QuickQuadraticRegression.cs
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //    Altaxo:  a data processing and data plotting program
-//    Copyright (C) 2002-2011 Dr. Dirk Lellinger
+//    Copyright (C) 2002-2025 Dr. Dirk Lellinger
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -41,6 +41,7 @@ namespace Altaxo.Calc.Regression
     private double _sy;
     private double _syx;
     private double _syxx;
+    private double _syy;
 
     /// <summary>
     /// Adds a data point to the regression.
@@ -55,6 +56,7 @@ namespace Altaxo.Calc.Regression
       _sxxx += (x * x) * x;
       _sxxxx += (x * x) * (x * x);
       _sy += y;
+      _syy += y * y;
       _syx += y * x;
       _syxx += y * x * x;
     }
@@ -72,11 +74,26 @@ namespace Altaxo.Calc.Regression
     }
 
     /// <summary>
+    /// Returns the number of entries added.
+    /// </summary>
+    public double N
+    {
+      get
+      {
+        return _n;
+      }
+    }
+
+    /// <summary>
     /// Gets the intercept value of the regression. Returns NaN if not enough data points entered.
     /// </summary>
     /// <returns>The intercept value or NaN if not enough data points are entered.</returns>
     public double GetA0()
     {
+      if (_n < 3)
+      {
+        throw new InvalidOperationException($"Insufficient number of points for quadratic regression: actual {_n}, but at least 3 needed.");
+      }
       return (-_sxxx * _sxxx * _sy + _sxx * _sxxxx * _sy + _sxx * _sxxx * _syx - _sx * _sxxxx * _syx - _sxx * _sxx * _syxx + _sx * _sxxx * _syxx) / GetDeterminant();
     }
 
@@ -86,6 +103,10 @@ namespace Altaxo.Calc.Regression
     /// <returns>The slope value or NaN if not enough data points are entered.</returns>
     public double GetA1()
     {
+      if (_n < 3)
+      {
+        throw new InvalidOperationException($"Insufficient number of points for quadratic regression: actual {_n}, but at least 3 needed.");
+      }
       return (_sxx * _sxxx * _sy - _sx * _sxxxx * _sy - _sxx * _sxx * _syx + _n * _sxxxx * _syx + _sx * _sxx * _syxx - _n * _sxxx * _syxx) / GetDeterminant();
     }
 
@@ -95,6 +116,10 @@ namespace Altaxo.Calc.Regression
     /// <returns>The slope value or NaN if not enough data points are entered.</returns>
     public double GetA2()
     {
+      if (_n < 3)
+      {
+        throw new InvalidOperationException($"Insufficient number of points for quadratic regression: actual {_n}, but at least 3 needed.");
+      }
       return (-_sxx * _sxx * _sy + _sx * _sxxx * _sy + _sx * _sxx * _syx - _n * _sxxx * _syx - _sx * _sx * _syxx + _n * _sxx * _syxx) / GetDeterminant();
     }
 
@@ -128,6 +153,187 @@ namespace Altaxo.Calc.Regression
       var a1 = GetA1();
       var a2 = GetA2();
       return new Func<double, double>(x => ((a2 * x) + a1) * x + a0);
+    }
+
+    /// <summary>
+    /// Get the residual sum of squares.
+    /// </summary>
+    /// <returns>The residual sum of squares, i.e. the sum of squared differences between original y and predicted y.</returns>
+    public double SumChiSquared()
+    {
+      var a0 = GetA0();
+      var a1 = GetA1();
+      var a2 = GetA2();
+      return _syy - a0 * _sy - a1 * _syx - a2 * _syxx;
+    }
+
+    /// <summary>
+    /// Returns the squared standard deviation of the regression.
+    /// </summary>
+    /// <returns>The squared standard deviation (variance) of the regression.</returns>
+    public double SigmaSquared()
+    {
+      return _n <= 3 ? double.NaN : SumChiSquared() / (_n - 3);
+    }
+
+    /// <summary>
+    /// Returns the standard deviation of the regression.
+    /// </summary>
+    /// <returns>The standard deviation of the regression.</returns>
+    public double Sigma()
+    {
+      var ss = SigmaSquared();
+      return ss < 0 ? 0 : Math.Sqrt(SigmaSquared());
+    }
+
+    /// <summary>
+    /// Gets the R squared value (square of the correlation coefficient).
+    /// </summary>
+    /// <returns>the R squared value (square of the correlation coefficient).</returns>
+    public double RSquared()
+    {
+      var SST = _syy - _sy * _sy / _n;
+      return 1 - SumChiSquared() / SST;
+    }
+
+    /// <summary>
+    /// Gets the adjusted R squared value.
+    /// </summary>
+    /// <returns>The adjusted R squared value.</returns>
+    public double AdjustedRSquared()
+    {
+      return _n <= 3 ? double.NaN : 1 - ((1 - RSquared()) * (_n - 1) / (_n - 3));
+    }
+
+    /// <summary>
+    /// Gets the covariance matrix.
+    /// </summary>
+    /// <returns>The covariance matrix.</returns>
+    public LinearAlgebra.Matrix<double> GetCovarianceMatrix()
+    {
+      var sq = SigmaSquared();
+      var cov = LinearAlgebra.Matrix<double>.Build.Dense(3, 3);
+
+      cov[0, 0] = _n;
+      cov[0, 1] = _sx;
+      cov[1, 0] = _sx;
+      cov[0, 2] = _sxx;
+      cov[2, 0] = _sxx;
+      cov[1, 1] = _sxx;
+      cov[1, 2] = _sxxx;
+      cov[2, 1] = _sxxx;
+      cov[2, 2] = _sxxxx;
+      cov = cov.Inverse();
+      cov[0, 0] *= sq;
+      cov[0, 1] *= sq;
+      cov[1, 0] *= sq;
+      cov[0, 2] *= sq;
+      cov[2, 0] *= sq;
+      cov[1, 1] *= sq;
+      cov[1, 2] *= sq;
+      cov[2, 1] *= sq;
+      cov[2, 2] *= sq;
+      return cov;
+    }
+
+    /// <summary>
+    /// Gets the prediction variance in dependence on x.
+    /// </summary>
+    /// <param name="x">The x value.</param>
+    /// <param name="covarianceMatrix">The covariance matrix. For repeated calls, get it in from <see cref="GetCovarianceMatrix"/>, otherwise, you can provide null.</param>
+    /// <returns>The prediction variance at the value x.</returns>
+    public double GetYVarianceOfX(double x, LinearAlgebra.Matrix<double>? covarianceMatrix = null)
+    {
+      covarianceMatrix ??= GetCovarianceMatrix();
+      return
+        covarianceMatrix[0, 0] +
+        x * (covarianceMatrix[0, 1] + covarianceMatrix[1, 0]) +
+        x * x * (covarianceMatrix[0, 2] + covarianceMatrix[2, 0]) +
+        x * x * covarianceMatrix[1, 1] +
+        x * x * x * (covarianceMatrix[1, 2] + covarianceMatrix[2, 1]) +
+        x * x * x * x * covarianceMatrix[2, 2];
+    }
+
+    /// <summary>
+    /// Gets the error of parameter A0.
+    /// </summary>
+    /// <param name="covarianceMatrix">The covariance matrix. For repeated calls, get it in from <see cref="GetCovarianceMatrix"/>, otherwise, you can provide null.</param>
+    /// <returns>The error of parameter A0.</returns>
+    public double GetA0Error(LinearAlgebra.Matrix<double>? covarianceMatrix = null)
+    {
+      covarianceMatrix ??= GetCovarianceMatrix();
+      var variance = covarianceMatrix[0, 0];
+      return variance < 0 ? 0 : Math.Sqrt(variance);
+    }
+
+    /// <summary>
+    /// Gets the error of parameter A1.
+    /// </summary>
+    /// <param name="covarianceMatrix">The covariance matrix.For repeated calls, get it in from <see cref="GetCovarianceMatrix"/>, otherwise, you can provide null.</param>
+    /// <returns>The error of parameter A1.</returns>
+    public double GetA1Error(LinearAlgebra.Matrix<double>? covarianceMatrix = null)
+    {
+      covarianceMatrix ??= GetCovarianceMatrix();
+      var variance = covarianceMatrix[1, 1];
+      return variance < 0 ? 0 : Math.Sqrt(variance);
+    }
+
+    /// <summary>
+    /// Gets the error of parameter A2.
+    /// </summary>
+    /// <param name="covarianceMatrix">The covariance matrix.For repeated calls, get it in from <see cref="GetCovarianceMatrix"/>, otherwise, you can provide null.</param>
+    /// <returns>The error of parameter A2.</returns>
+    public double GetA2Error(LinearAlgebra.Matrix<double>? covarianceMatrix = null)
+    {
+      covarianceMatrix ??= GetCovarianceMatrix();
+      var variance = covarianceMatrix[2, 2];
+      return variance < 0 ? 0 : Math.Sqrt(variance);
+    }
+
+    /// <summary>
+    /// Gets the mean prediction error of y in dependence on x.
+    /// </summary>
+    /// <param name="x">The x value.</param>
+    /// <param name="covarianceMatrix">The covariance matrix. For repeated calls, get it in from <see cref="GetCovarianceMatrix"/>, otherwise, you can provide null.</param>
+    /// <returns>The mean prediction error of y in dependence on x.</returns>
+    public double GetYErrorOfX(double x, LinearAlgebra.Matrix<double>? covarianceMatrix = null)
+    {
+      var variance = GetYVarianceOfX(x, covarianceMatrix);
+      return variance < 0 ? 0 : Math.Sqrt(variance);
+    }
+
+    /// <summary>
+    /// Gets the confidence band of the prediction.
+    /// </summary>
+    /// <param name="x">The x value.</param>
+    /// <param name="covarianceMatrix">The covariance matrix. For repeated calls, get it in from <see cref="GetCovarianceMatrix"/>, otherwise, you can provide null.</param>
+    /// <param name="confidenceLevel">The confidence level.</param>
+    /// <returns>The lower value of the confidence band, the mean value of the prediction, and the upper value of the confidence band.</returns>
+    public (double yLower, double yMean, double yUpper) GetConfidenceBand(double x, double confidenceLevel, LinearAlgebra.Matrix<double>? covarianceMatrix = null)
+    {
+      covarianceMatrix ??= GetCovarianceMatrix();
+      var t = Altaxo.Calc.Probability.StudentsTDistribution.Quantile(1 - (0.5 * (1 - confidenceLevel)), _n - 3);
+      var et = t * GetYErrorOfX(x, covarianceMatrix);
+      var y = GetYOfX(x);
+      return (y - et, y, y + et);
+    }
+
+    /// <summary>
+    /// Gets the confidence band of the prediction for multiple x-values.
+    /// </summary>
+    /// <param name="xdata">The x values.</param>
+    /// <param name="confidenceLevel">The confidence level.</param>
+    /// <returns>Enumeration with the x values, the lower value of the confidence band, the mean value of the prediction, and the upper value of the confidence band.</returns>
+    public IEnumerable<(double x, double yLower, double yMean, double yUpper)> GetConfidenceBand(IEnumerable<double> xdata, double confidenceLevel)
+    {
+      var t = Altaxo.Calc.Probability.StudentsTDistribution.Quantile(1 - (0.5 * (1 - confidenceLevel)), _n - 3);
+      var cov = GetCovarianceMatrix();
+      foreach (var x in xdata)
+      {
+        var et = t * GetYErrorOfX(x, cov);
+        var y = GetYOfX(x);
+        yield return (x, y - et, y, y + et);
+      }
     }
   }
 }
---
+++ Altaxo/CoreTest/Calc/Regression/QuickQuadraticRegressionTests.cs
@@ -0,0 +1,116 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2025 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+using Xunit;
+
+namespace Altaxo.Calc.Regression.Foo
+{
+  public class QuickQuadraticRegressionTests
+  {
+    private static (double x, double y)[] _data = new (double x, double y)[] { (0, 1), (0, 4), (1, 3), (1, 6), (2, -1), (2, 2) };
+
+    [Fact]
+    public void TestInterceptAndSlope()
+    {
+      const double expectedA0 = 2.5;
+      const double expectedA1 = 5.0;
+      const double expectedA2 = -3.0;
+      const double expectedSumOfChiSquared = 6 * 1.5 * 1.5;
+
+      var qlr = new QuickQuadraticRegression();
+      foreach (var (x, y) in _data)
+      {
+        qlr.Add(x, y);
+      }
+      double a0 = qlr.GetA0();
+      double a1 = qlr.GetA1();
+
+      AssertEx.AreEqual(expectedA0, a0, 1e-10, 0.0, "Intercept test");
+      AssertEx.AreEqual(expectedA1, a1, 1e-10, 0.0, "Slope test");
+      AssertEx.AreEqual(expectedA2, qlr.GetA2(), 1e-10, 0.0, "Second slope test");
+
+      AssertEx.AreEqual(expectedA0 + expectedA1 * 0 + expectedA2 * 0 * 0, qlr.GetYOfX(0), 1e-10, 0.0, "Y of X=0 test");
+      AssertEx.AreEqual(expectedA0 + expectedA1 * 1 + expectedA2 * 1 * 1, qlr.GetYOfX(1), 1e-10, 0.0, "Y of X=1 test");
+      AssertEx.AreEqual(expectedA0 + expectedA1 * 2 + expectedA2 * 2 * 2, qlr.GetYOfX(2), 1e-10, 0.0, "Y of X=2 test");
+
+      AssertEx.AreEqual(expectedSumOfChiSquared, qlr.SumChiSquared(), 1e-10, 0.0, "Chi squared test");
+      AssertEx.AreEqual(expectedSumOfChiSquared / (qlr.N - 3), qlr.SigmaSquared(), 1e-10, 0.0, "Sigma squared test");
+      AssertEx.AreEqual(Math.Sqrt(expectedSumOfChiSquared / (qlr.N - 3)), qlr.Sigma(), 1e-10, 0.0, "Sigma test");
+
+
+
+      // Calculate the sum of squared errors
+      double chiSquared = 0.0;
+      foreach (var (x, y) in _data)
+      {
+        double yPred = qlr.GetYOfX(x);
+        chiSquared += (y - yPred) * (y - yPred);
+      }
+      AssertEx.AreEqual(expectedSumOfChiSquared, chiSquared, 1e-10, 0.0, "Chi squared test");
+    }
+
+    [Fact]
+    public void TestInsufficientDataPoints()
+    {
+      var qlr = new QuickQuadraticRegression();
+      qlr.Add(1, 2);
+      qlr.Add(2, 3);
+      // Only two points, quadratic regression is not possible
+      Assert.Throws<System.InvalidOperationException>(() => qlr.GetA0());
+      Assert.Throws<System.InvalidOperationException>(() => qlr.GetA1());
+      Assert.Throws<System.InvalidOperationException>(() => qlr.GetA2());
+    }
+
+    [Fact]
+    public void TestCovarianceMatrix()
+    {
+      var qlr = new QuickQuadraticRegression();
+      foreach (var (x, y) in _data)
+      {
+        qlr.Add(x, y);
+      }
+      var cov = qlr.GetCovarianceMatrix();
+      // Check shape
+      Assert.Equal(3, cov.RowCount);
+      Assert.Equal(3, cov.ColumnCount);
+      // Check symmetry
+      for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+          AssertEx.AreEqual(cov[i, j], cov[j, i], 1e-12, 0.0, $"Covariance symmetry [{i},{j}]");
+      // Check non-negative variances
+      for (int i = 0; i < 3; ++i)
+        Assert.True(cov[i, i] >= 0, $"Variance at [{i},{i}] should be non-negative");
+
+      AssertEx.AreEqual(9 / 4d, cov[0, 0], 1E-10, 0);
+      AssertEx.AreEqual(-27 / 8d, cov[0, 1], 1E-10, 0);
+      AssertEx.AreEqual(9 / 8d, cov[0, 2], 1E-10, 0);
+      AssertEx.AreEqual(117 / 8d, cov[1, 1], 1E-10, 0);
+      AssertEx.AreEqual(-27 / 4d, cov[1, 2], 1E-10, 0);
+      AssertEx.AreEqual(27 / 8d, cov[2, 2], 1E-10, 0);
+
+
+    }
+  }
+}
