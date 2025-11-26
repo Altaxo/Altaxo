1985d2fe00d0058472f0f0bf19e80c0e154d49a9
Dirk Lellinger <dlellinger@users.sourceforge.net>
2025-11-27 00:03:34 +0100

    Add LambertW function

---
+++ Altaxo/Core/Calc/LambertW.cs
@@ -0,0 +1,109 @@
+﻿#region Copyright
+
+/////////////////////////////////////////////////////////////////////////////
+//    Altaxo:  a data processing and data plotting program
+//    Copyright (C) 2002-2025 Dr. Dirk Lellinger
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#endregion Copyright
+
+using System;
+
+namespace Altaxo.Calc
+{
+  /// <summary>
+  /// Provides static methods for evaluating branches of the Lambert W function for real arguments.
+  /// </summary>
+  /// <remarks>The Lambert W function, also known as the product logarithm, is the set of functions W(z)
+  /// satisfying W(z) ⋅ e^{W(z)} = z. This class offers methods to compute the principal branch (W0) and the W_{-1}
+  /// branch for real values of z. All methods are static and thread-safe.</remarks>
+  public static class LambertW
+  {
+    /// <summary>
+    /// Computes the principal branch (W0) of the Lambert W function for a given value.
+    /// </summary>
+    /// <remarks>The principal branch W0 is real-valued for z &gt;= -1/e. For values of z &lt; -1/e, the result may
+    /// be complex or undefined. The method uses an initial approximation followed by iterative refinement to achieve
+    /// the specified tolerance.</remarks>
+    /// <param name="z">The input value for which to evaluate the principal branch of the Lambert W function.</param>
+    /// <param name="maxIter">The maximum number of iterations to perform when refining the result. Must be positive. The default is 50.</param>
+    /// <param name="tol">The convergence tolerance for the iterative refinement. Must be greater than zero. The default is 1e-12.</param>
+    /// <returns>The value of the principal branch of the Lambert W function at the specified input.</returns>
+    public static double W0(double z, int maxIter = 50, double tol = 1e-12)
+    {
+      // Startwert: asymptotische Näherung für große z
+      double w = (z > 3.0)
+          ? Math.Log(z) - Math.Log(Math.Log(z))
+          : z; // für kleine z reicht Taylor-Näherung
+
+      return Newton(z, w, maxIter, tol);
+    }
+
+    /// <summary>
+    /// Computes the W_{-1} branch of the Lambert W function for real arguments in the range -1/e %le; z &lt; 0.
+    /// </summary>
+    /// <remarks>The W_{-1} branch of the Lambert W function is defined only for real z in the interval -1/e &lt;=
+    /// z &lt; 0. This method uses an iterative approach and may not converge for values of z very close to -1/e or for
+    /// insufficiently large maxIter or loose tol values.</remarks>
+    /// <param name="z">The input value for which to compute the W_{-1} branch. Must satisfy -1/e &lt;= z &lt; 0.</param>
+    /// <param name="maxIter">The maximum number of iterations to use when refining the result. Must be positive. The default is 50.</param>
+    /// <param name="tol">The convergence tolerance for the iterative solver. Must be positive. The default is 1e-12.</param>
+    /// <returns>The value w such that w ⋅ e^{w} = z, where w is on the W_{-1} branch. Returns a real number for valid z in the
+    /// range -1/e &lt;= z &lt; 0.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when z is outside the valid range of -1/e &lt;= z &lt; 0.</exception>
+    public static double Wm1(double z, int maxIter = 50, double tol = 1e-12)
+    {
+      if (z > 0 || z < -1.0 / Math.E)
+        throw new ArgumentOutOfRangeException(nameof(z), "Nur für -1/e <= z < 0 definiert.");
+
+      // Start value: asymptotic approximation for z close to 0
+      double w = Math.Log(-z) - Math.Log(-Math.Log(-z));
+
+      return Newton(z, w, maxIter, tol);
+    }
+
+    /// <summary>
+    /// Finds a root of the equation w * exp(w) = z using the Newton-Raphson method.
+    /// </summary>
+    /// <remarks>If the method does not converge within the specified number of iterations, the last computed
+    /// value is returned. The accuracy of the result depends on the choice of the initial guess and the
+    /// tolerance.</remarks>
+    /// <param name="z">The value for which to solve the equation w * exp(w) = z.</param>
+    /// <param name="w">The initial guess for the root.</param>
+    /// <param name="maxIter">The maximum number of iterations to perform.</param>
+    /// <param name="tol">The convergence tolerance. The iteration stops when the change between successive approximations is less than
+    /// this value.</param>
+    /// <returns>The computed root approximation for w such that w * exp(w) ≈ z.</returns>
+    private static double Newton(double z, double w, int maxIter, double tol)
+    {
+      for (int i = 0; i < maxIter; i++)
+      {
+        double ew = Math.Exp(w);
+        double f = w * ew - z;
+        double fp = ew * (w + 1);
+        double wNext = w - f / fp;
+
+        if (Math.Abs(wNext - w) < tol)
+          return wNext;
+
+        w = wNext;
+      }
+      return w;
+    }
+  }
+}
---
+++ Altaxo/CoreTest/Calc/LambertWTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Altaxo.Calc
+{
+  public class LambertWTests
+  {
+    [Fact]
+    /// <summary>
+    /// Test values for W0 branch, positive arguments
+    /// </summary>
+    public void LambertW_W0_TestValues_Positive()
+    {
+      var testValues = new Dictionary<double, double>
+      {
+        { 0, 0 },
+        { 1E-12, 9.99999999999E-13},
+        { 1E-4, 0.000099990001499733385406},
+        { 1, 0.56714329040978387300 },
+        { 2, 0.85260550201372549135 },
+        { Math.E, 1 },
+        { 10, 1.7455280027406993831 },
+        { 100, 3.3856301402900501849 },
+      };
+      foreach (var kvp in testValues)
+      {
+        double z = kvp.Key;
+        double expected = kvp.Value;
+        double result = LambertW.W0(z);
+        AssertEx.AreEqual(expected, result, 0, 1E-12);
+      }
+    }
+  }
+}
